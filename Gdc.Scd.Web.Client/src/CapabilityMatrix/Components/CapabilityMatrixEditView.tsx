11d6590040981f802baf0918942820c3c5fcdc94
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-07-17 17:31:07 +0300

    Add multiview control Add layout

--- Gdc.Scd.Web.Client/src/CapabilityMatrix/Components/CapabilityMatrixEditView.tsx
+++ Gdc.Scd.Web.Client/src/CapabilityMatrix/Components/CapabilityMatrixEditView.tsx
@@ -1,9 +1,10 @@
 ﻿import * as React from "react";
-import { Container, Button, FormPanel, CheckBoxField, SelectField, ExtReact, List } from "@extjs/ext-react";
+import { Container, Button, CheckBoxField, SelectField, List, Label, ComboBoxField } from "@extjs/ext-react";
+import { CapabilityMatrixMultiSelect } from "./CapabilityMatrixMultiSelect";
 
 export class CapabilityMatrixEditView extends React.Component<{}> {
 
-    store = new Ext.data.Store({
+    store = Ext.create('Ext.data.Store', {
         data: [
             { title: 'Item 1' },
             { title: 'Item 2' },
@@ -12,28 +13,59 @@ export class CapabilityMatrixEditView extends React.Component<{}> {
         ]
     });
 
+    countries = [
+        { "name": "Alabama", "abbrev": "AL" },
+        { "name": "Alaska", "abbrev": "AK" },
+        { "name": "Arizona", "abbrev": "AZ" }
+    ];
+
+    onCountryChange() {
+        console.log('onCountryChange()', new Date().getTime());
+    }
+
     public render() {
+
+        let isPortfolio = true;
+
         return (
-            <Container layout="vbox">
-                <Container>
+            <Container layout="vbox" padding="10px">
 
-                    <ExtReact>
-                        <List
-                            itemTpl="{title}"
-                            store={this.store}
-                        />
-                    </ExtReact>
+                <ComboBoxField
+                    width="250px"
+                    label="Country:"
+                    labelAlign="left"
+                    labelWidth="80px"
+                    options={this.countries}
+                    displayField="name"
+                    valueField="code"
+                    queryMode="local"
+                    typeAhead
+                    clearable="true"
 
-                    <FormPanel shadow layout={{ type: 'vbox', align: 'left' }}>
-                        <CheckBoxField boxLabel="Fujitsu Global Portfolio" />
-                        <CheckBoxField boxLabel="Master Portfolio" />
-                        <CheckBoxField boxLabel="Core Portfolio" />
-                        <Container>
-                            <Button text="Deny combinations" ui="decline" />
-                            <Button text="Allow combinations" />
-                        </Container>
-                    </FormPanel>
+                    onChange={this.onCountryChange}
+
+                />
+
+                <Container layout="hbox">
+                    <CapabilityMatrixMultiSelect title="Asset(WG)" itemTpl="{title}" store={this.store} />
+                    <CapabilityMatrixMultiSelect title="Availability" itemTpl="{title}" store={this.store} />
+                    <CapabilityMatrixMultiSelect title="Duration" itemTpl="{title}" store={this.store} />
+                    <CapabilityMatrixMultiSelect title="React type" itemTpl="{title}" store={this.store} />
+                    <CapabilityMatrixMultiSelect title="Reaction time" itemTpl="{title}" store={this.store} />
+                    <CapabilityMatrixMultiSelect title="Service location" itemTpl="{title}" store={this.store} />
                 </Container>
+
+                <Container layout={{ type: 'vbox', align: 'left' }} defaults={{ disabled: isPortfolio }}>
+                    <CheckBoxField boxLabel="Fujitsu Global Portfolio" />
+                    <CheckBoxField boxLabel="Master Portfolio" />
+                    <CheckBoxField boxLabel="Core Portfolio" />
+                </Container>
+
+                <Container>
+                    <Button text="Deny combinations" ui="decline" padding="0 10px 0 0" />
+                    <Button text="Allow combinations" />
+                </Container>
+
             </Container>
         );
     }
---
+++ Gdc.Scd.Web.Client/src/CapabilityMatrix/Components/CapabilityMatrixMultiSelect.tsx
@@ -0,0 +1,23 @@
+﻿import * as React from "react";
+import { Container, Label, List } from "@extjs/ext-react";
+
+export class CapabilityMatrixMultiSelect extends React.Component<any> {
+
+    public render() {
+
+        let { width, maxWidth, title, itemTpl, store } = this.props;
+
+        title = '<h4>' + title + '</h4>';
+
+        width = width || '100%';
+        maxWidth = maxWidth || '200px';
+
+        return (
+            <Container width={width} maxWidth={maxWidth}>
+                <Label html={title} padding="7px" />
+                <List itemTpl={itemTpl} store={store} selectable="multi" scrollable="true" />
+            </Container>
+        );
+    }
+
+}
