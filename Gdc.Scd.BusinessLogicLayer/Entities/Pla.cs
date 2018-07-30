3b5f830ef957f10b1b84b0f7d23c5ef28d246624
Timur Rizvanov <Timur.Rizvanov.gdc@ts.fujitsu.com>
2018-07-30 11:01:19 +0300

    Test data was improved by Pla.

---
+++ Gdc.Scd.BusinessLogicLayer/Entities/Pla.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Gdc.Scd.Core.Entities;
+using Gdc.Scd.Core.Meta.Constants;
+
+namespace Gdc.Scd.BusinessLogicLayer.Entities
+{
+    [Table("Pla", Schema = MetaConstants.InputLevelSchema)]
+    public class Pla : NamedId
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override long Id
+        {
+            get => base.Id;
+            set => base.Id = value;
+        }
+
+        public List<Wg> WarrantyGroups { get; set; }
+    }
+}
