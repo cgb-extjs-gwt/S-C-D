061cfe5648d35c4eaf68cdbba36c2797c61eba0a
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-08-09 17:17:39 +0300

    Implement builder pattern for sqlparameterbuilder

--- Gdc.Scd.BusinessLogicLayer/Procedures/DelMatrixRules.cs
+++ Gdc.Scd.BusinessLogicLayer/Procedures/DelMatrixRules.cs
@@ -28,7 +28,7 @@ namespace Gdc.Scd.BusinessLogicLayer.Procedures
 
         private DbParameter Prepare(long[] items)
         {
-            return SqlParameterBuilder.CreateListID("@rules", items);
+            return new SqlParameterBuilder().WithName("@rules").WithListIdValue(items).Build();
         }
     }
 }
