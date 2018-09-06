35f2a3e1a98c7d49cab008f6e17968df6401d1aa
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-09-06 17:33:15 +0300

    add sog, year dict controllers

---
+++ Gdc.Scd.Web.Api/Controllers/Dict/SogController.cs
@@ -0,0 +1,11 @@
+﻿using Gdc.Scd.BusinessLogicLayer.Interfaces;
+using Gdc.Scd.Core.Entities;
+
+namespace Gdc.Scd.Web.Api.Controllers.Dict
+{
+    public class SogController : BaseDomainController<Sog>
+    {
+        public SogController(IDomainService<Sog> domainService)
+            : base(domainService) { }
+    }
+}
