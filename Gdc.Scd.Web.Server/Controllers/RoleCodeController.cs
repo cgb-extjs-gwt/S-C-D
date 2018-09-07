e7a7db6efab24adbaa70d91f7ecfd7d7d345088c
Evgenia Borisova <evgenia.borisova.gdc@ts.fujitsu.com>
2018-09-07 16:26:18 +0300

    Fixed missing controllers.

---
+++ Gdc.Scd.Web.Server/Controllers/RoleCodeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gdc.Scd.Core.Entities;
+using Gdc.Scd.BusinessLogicLayer.Interfaces;
+
+namespace Gdc.Scd.Web.Server.Controllers
+{
+    public class RoleCodeController : BaseDomainController<RoleCode>
+    {
+        public RoleCodeController(IDomainService<RoleCode> domainService):base(domainService)
+        {         
+        }
+    }
+}
