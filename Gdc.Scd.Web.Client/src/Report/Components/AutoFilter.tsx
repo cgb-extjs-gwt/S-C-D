23958adf3b5120fb55bbc1ec7b29b525a23744a7
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-09-17 17:23:54 +0300

    add auto grid with auto filter

---
+++ Gdc.Scd.Web.Client/src/Report/Components/AutoFilter.tsx
@@ -0,0 +1,62 @@
+﻿import { Button, Container, Panel, PanelProps, TextField } from "@extjs/ext-react";
+import * as React from "react";
+
+export interface AutoFilterPanelProps extends PanelProps {
+    filter(): React.Component[];
+    onSearch(filter: any): void;
+}
+
+export class AutoFilter extends React.Component<AutoFilterPanelProps, any> {
+
+    public constructor(props: any) {
+        super(props);
+        this.init();
+    }
+
+    public render() {
+        return (
+            <Panel {...this.props} margin="0 0 5px 0" padding="4px 20px 7px 20px">
+
+                <Container margin="10px 0"
+                    defaults={{
+                        maxWidth: '200px',
+                        valueField: 'id',
+                        displayField: 'name',
+                        queryMode: 'local',
+                        clearable: 'true'
+                    }}>
+
+                    {this.props.filter()}
+
+                </Container>
+
+                <Button text="Search" ui="action" width="85px" handler={this.onSearch} margin="20px auto" />
+
+            </Panel>
+        );
+    }
+
+    public getModel(): any {
+        let result = {};
+
+        let fields = this.props.filter();
+
+        for (let i = 0, f: any; f = fields[i]; i++) {
+            result[f.getLabel()] = f.getValue();
+        }
+
+        return result;
+    }
+
+    private init() {
+        this.onSearch = this.onSearch.bind(this);
+    }
+
+    private onSearch() {
+        let handler = this.props.onSearch;
+        if (handler) {
+            handler(this.getModel());
+        }
+    }
+
+}
---
+++ Gdc.Scd.Web.Client/src/Report/Components/AutoGrid.tsx
@@ -0,0 +1,86 @@
+﻿import { Column, Container, Grid } from "@extjs/ext-react";
+import * as React from "react";
+import { AutoFilter } from "./AutoFilter";
+
+export abstract class AutoGrid extends React.Component<any, any> {
+
+    private grid: Grid;
+
+    private filter: AutoFilter;
+
+    private store: Ext.data.IStore = Ext.create('Ext.data.Store', {
+
+        pageSize: 25,
+
+        autoLoad: true,
+
+        proxy: {
+            type: 'ajax',
+            api: {
+                read: this.getUrl()
+            },
+            reader: {
+                type: 'json',
+                rootProperty: 'items',
+                totalProperty: 'total'
+            }
+        }
+    });
+
+    public constructor(props: any) {
+        super(props);
+        this.init();
+    }
+
+    public abstract getColumns(): Column[];
+
+    public abstract getFilter(): React.Component[];
+
+    public abstract getUrl(): string;
+
+    public render() {
+        return (
+            <Container layout="fit">
+
+                <AutoFilter ref="filter" docked="right" filter={this.getFilter} onSearch={this.onSearch} />
+
+                <Grid
+                    ref="grid"
+                    store={this.store}
+                    width="100%"
+                    plugins={['pagingtoolbar']}>
+
+                    {this.getColumns()}
+
+                </Grid>
+
+            </Container>
+        );
+    }
+
+    public componentDidMount() {
+        this.grid = this.refs.grid as Grid;
+        this.filter = this.refs.filter as AutoFilter;
+    }
+
+    private init() {
+        this.onSearch = this.onSearch.bind(this);
+        this.onBeforeLoad = this.onBeforeLoad.bind(this);
+
+        this.store.on('beforeload', this.onBeforeLoad);
+    }
+
+    private onSearch(filter: any) {
+        this.reload();
+    }
+
+    private reload() {
+        this.store.load();
+    }
+
+    private onBeforeLoad(s, operation) {
+        let filter = this.filter.getModel();
+        let params = Ext.apply({}, operation.getParams(), filter);
+        operation.setParams(params);
+    }
+}
