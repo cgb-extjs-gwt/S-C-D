87d282981e701ab85e570d3e06620a0246a99a50
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-10-04 16:28:20 +0300

    add Report.SolutionPackPriceList

---
+++ Gdc.Scd.DataAccessLayer.TestData/Scripts/report-solution-pack-price-list.sql
@@ -0,0 +1,69 @@
+﻿IF OBJECT_ID('Report.SolutionPackPriceList') IS NOT NULL
+  DROP FUNCTION Report.SolutionPackPriceList;
+go 
+
+CREATE FUNCTION Report.SolutionPackPriceList
+(
+    @wg bigint
+)
+RETURNS TABLE 
+AS
+RETURN (
+    select 
+              wg.SogDescription
+            , wg.Sog
+            , wg.Name as Wg
+            , fsp.Name
+
+            , null as SpDescription
+            , null as Sp
+
+            , null as TP
+            , null as DealerPrice
+            , null as ListPrice
+
+    from InputAtoms.WgSogView wg
+    join Fsp.HwFspCodeTranslation fsp on fsp.WgId = wg.Id
+    where (@wg is null or wg.Id = @wg)
+)
+
+GO
+
+declare @reportId bigint = (select Id from Report.Report where upper(Name) = 'SOLUTIONPACK-PRICE-LIST');
+declare @index int = 0;
+
+delete from Report.ReportColumn where ReportId = @reportId;
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'SogDescription', 'Infrastructure Solution', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Sog', 'Service Offering Group', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'WgDescription', 'Infrastructure Solution', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Wg', 'Warranty Group', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Fsp', 'SolutionPack Product no.', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'SpDescription', 'SolutionPack Service Description', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Sp', 'SolutionPack Service Short Description', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'TP', 'Transfer Price', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'DealerPrice', 'Dealer Price (Central Reference)', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'ListPrice', 'List Price (Central Reference)', 1, 1);
+
+set @index = 0;
+
+delete from Report.ReportFilter where ReportId = @reportId;
+
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, 4, 'wg', 'Warranty Group');
+
+GO
