4c153de5f0fe369fb6b9cf018d9b2099805f2057
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-10-09 17:42:00 +0300

    add all selection for multiview

--- Gdc.Scd.Web.Client/src/CapabilityMatrix/Components/MultiSelect.tsx
+++ Gdc.Scd.Web.Client/src/CapabilityMatrix/Components/MultiSelect.tsx
@@ -1,5 +1,5 @@
-﻿import * as React from "react";
-import { Container, Label, List } from "@extjs/ext-react";
+﻿import { CheckBoxField, Container, List } from "@extjs/ext-react";
+import * as React from "react";
 import { ExtDataviewHelper } from "../../Common/Helpers/ExtDataviewHelper";
 
 export interface MultiSelectProps {
@@ -25,6 +25,11 @@ export class MultiSelect extends React.Component<MultiSelectProps> {
 
     private lst: List;
 
+    public constructor(props: MultiSelectProps) {
+        super(props);
+        this.init();
+    }
+
     public render() {
 
         let { width, maxWidth, height, maxHeight, title, itemTpl, store, selectable } = this.props;
@@ -40,7 +45,7 @@ export class MultiSelect extends React.Component<MultiSelectProps> {
 
         return (
             <Container width={width} maxWidth={maxWidth}>
-                <Label html={title} padding="7px" />
+                <CheckBoxField boxLabel={title} padding="7px" bodyAlign="left" onChange={this.onTopSelectionChange} />
                 <List ref="lst" itemTpl={itemTpl} store={store} height={height} maxHeight={maxHeight} selectable={selectable} scrollable="true" />
             </Container>
         );
@@ -56,5 +61,19 @@ export class MultiSelect extends React.Component<MultiSelectProps> {
 
     public getSelectedKeys<T>(field: string): T[] {
         return ExtDataviewHelper.getListSelected(this.lst, field);
+    }
+
+    private init() {
+        this.onTopSelectionChange = this.onTopSelectionChange.bind(this);
+    }
+
+    private onTopSelectionChange(cb: CheckBoxField, newVal: boolean, oldVal: boolean) {
+        let view = this.lst as any;
+        if (newVal) {
+            view.selectAll();
+        }
+        else {
+            view.deselectAll();
+        }
     }
 }
