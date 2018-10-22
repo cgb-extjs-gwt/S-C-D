c9a5730ba73ca3e039753f47a5417da776c3edee
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-10-22 13:00:57 +0300

    fix scripts

--- Gdc.Scd.DataAccessLayer/DataBaseScripts/matrix/hw-calc-gen.sql
+++ Gdc.Scd.DataAccessLayer/DataBaseScripts/matrix/hw-calc-gen.sql
@@ -1,24 +1,52 @@
+IF OBJECT_ID('tempdb..#ShrinkLog') IS NOT NULL DROP PROC #ShrinkLog
+
+GO
+
+CREATE PROC #ShrinkLog
+AS
+    DBCC SHRINKFILE ('Scd_2_log', 1);
+    RETURN 0
+GO
+
 -- Disable all table constraints
 ALTER TABLE Hardware.ServiceCostCalculation NOCHECK CONSTRAINT ALL
 
 DELETE FROM Hardware.ServiceCostCalculation;
 
-DBCC SHRINKDATABASE (Scd_2_3, 50);
+exec #ShrinkLog;
 GO
 
-INSERT INTO Hardware.ServiceCostCalculation (MatrixId) (
-	SELECT m.Id
-	FROM Matrix.Matrix m
-    where m.CountryId is not null
-);
+declare @rownum int = 1;
+declare @cnt bigint;
+declare @flag bit = 1;
 
-DBCC SHRINKDATABASE (Scd_2_3, 50);
-GO
+SELECT ROW_NUMBER() over(order by cnt.Id) as rownum, cnt.Id
+INTO #Temp_Cnt
+FROM InputAtoms.Country cnt
+where cnt.IsMaster = 1
+
+while @flag = 1
+begin
+    set @flag = 0;
+    select @flag = 1, @cnt = Id from #Temp_Cnt where rownum = @rownum;
+    set @rownum = @rownum + 1;
+
+    if @flag = 0 break;
+
+    INSERT INTO Hardware.ServiceCostCalculation (MatrixId) (
+	    SELECT m.Id
+	    FROM Matrix.Matrix m
+        where m.CountryId = @cnt
+    );
+
+    exec #ShrinkLog;
+end;
+
+----------------------------------------------------------------------------------
 
 -- Enable all table constraints
 ALTER TABLE Hardware.ServiceCostCalculation CHECK CONSTRAINT ALL
-GO  
-
-DBCC SHRINKDATABASE (Scd_2_3, 50);
+exec #ShrinkLog;
 GO
 
+IF OBJECT_ID('tempdb..#ShrinkLog') IS NOT NULL DROP PROC #ShrinkLog
---
+++ Gdc.Scd.DataAccessLayer/DataBaseScripts/matrix/matrix-country-gen.sql
@@ -0,0 +1,104 @@
+IF OBJECT_ID('tempdb..#Temp_SLA') IS NOT NULL DROP TABLE #Temp_SLA
+IF OBJECT_ID('tempdb..#Temp_Cnt') IS NOT NULL DROP TABLE #Temp_Cnt
+IF OBJECT_ID('tempdb..#ShrinkLog') IS NOT NULL DROP PROC #ShrinkLog
+
+GO
+
+CREATE PROC #ShrinkLog
+AS
+    DBCC SHRINKFILE ('Scd_2_log', 1);
+    RETURN 0
+GO
+
+ALTER INDEX IX_Matrix_AvailabilityId ON Matrix.Matrix DISABLE;  
+ALTER INDEX IX_Matrix_DurationId ON Matrix.Matrix DISABLE;  
+ALTER INDEX IX_Matrix_ReactionTimeId ON Matrix.Matrix DISABLE;  
+ALTER INDEX IX_Matrix_ReactionTypeId ON Matrix.Matrix DISABLE;  
+ALTER INDEX IX_Matrix_ServiceLocationId ON Matrix.Matrix DISABLE;  
+ALTER INDEX IX_Matrix_CountryId ON Matrix.Matrix DISABLE;  
+ALTER INDEX IX_Matrix_WgId ON Matrix.Matrix DISABLE;  
+
+-- Disable all table constraints
+ALTER TABLE Matrix.Matrix NOCHECK CONSTRAINT ALL
+
+DELETE FROM Matrix.Matrix;
+
+exec #ShrinkLog
+GO
+
+SELECT wg.Id AS wg, 
+		av.Id AS av, 
+		dur.Id AS dur, 
+		rtype.Id AS reacttype, 
+		rtime.Id AS reacttime,
+		sv.Id AS srvloc
+INTO #Temp_Sla
+FROM InputAtoms.Wg AS wg
+CROSS JOIN Dependencies.Availability AS av
+CROSS JOIN Dependencies.Duration AS dur
+CROSS JOIN Dependencies.ReactionType AS rtype
+CROSS JOIN Dependencies.ReactionTime AS rtime
+CROSS JOIN Dependencies.ServiceLocation AS sv
+
+declare @rownum int = 1;
+declare @cnt bigint;
+declare @flag bit = 1;
+
+SELECT ROW_NUMBER() over(order by cnt.Id) as rownum, cnt.Id
+INTO #Temp_Cnt
+FROM InputAtoms.Country cnt
+where cnt.IsMaster = 1
+
+while @flag = 1
+begin
+    set @flag = 0;
+    select @flag = 1, @cnt = Id from #Temp_Cnt where rownum = @rownum;
+    set @rownum = @rownum + 1;
+
+    if @flag = 0 break;
+
+    INSERT INTO Matrix.Matrix (CountryId, WgId, AvailabilityId, DurationId, ReactionTypeId, ReactionTimeId, ServiceLocationId, Denied) (
+
+	    SELECT @cnt, sla.wg, sla.av, sla.dur, sla.reacttype, sla.reacttime, sla.srvloc, 0
+	    FROM #Temp_Sla sla
+    );
+
+    exec #ShrinkLog
+end;
+
+ALTER INDEX IX_Matrix_AvailabilityId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+ALTER INDEX IX_Matrix_DurationId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+ALTER INDEX IX_Matrix_ReactionTimeId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+ALTER INDEX IX_Matrix_ReactionTypeId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+ALTER INDEX IX_Matrix_ServiceLocationId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+ALTER INDEX IX_Matrix_CountryId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+ALTER INDEX IX_Matrix_WgId ON Matrix.Matrix REBUILD;  
+exec #ShrinkLog
+GO  
+
+-- Enable all table constraints
+ALTER TABLE Matrix.Matrix CHECK CONSTRAINT ALL
+exec #ShrinkLog
+GO
+
+IF OBJECT_ID('tempdb..#Temp_SLA') IS NOT NULL DROP TABLE #Temp_SLA
+IF OBJECT_ID('tempdb..#Temp_Cnt') IS NOT NULL DROP TABLE #Temp_Cnt
+IF OBJECT_ID('tempdb..#ShrinkLog') IS NOT NULL DROP PROC #ShrinkLog
