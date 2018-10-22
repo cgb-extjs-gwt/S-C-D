44e32bd5bad3c1c89f8f2b7d073c4b44019578f9
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-10-22 11:29:00 +0300

    fix scripts

--- Gdc.Scd.DataAccessLayer.TestData/Impl/TestDataCreationHandlercs.cs
+++ Gdc.Scd.DataAccessLayer.TestData/Impl/TestDataCreationHandlercs.cs
@@ -99,7 +99,12 @@ namespace Gdc.Scd.DataAccessLayer.TestData.Impl
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-hdd-retention-parameter.sql"));
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-local-detailed.sql"));
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-locap.sql"));
+            queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-Logistic-cost-calc-central.sql"));
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-logistic-cost-calc-country.sql"));
+            queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-logistic-cost-central.sql"));
+            queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-logistic-cost-country.sql"));
+            queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-Logistic-cost-input-central.sql"));
+            queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-Logistic-cost-input-country.sql"));
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-po-standard-warranty.sql"));
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-proactive.sql"));
             queries.AddRange(this.BuildFromFile(@"Scripts.Report.report-solution-pack-price-list-detail.sql"));
