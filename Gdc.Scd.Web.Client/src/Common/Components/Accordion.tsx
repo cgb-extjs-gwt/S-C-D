4546a31be97aac493edc8547d81e3f33203b72e0
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-11-16 17:48:12 +0300

    add accordion

---
+++ Gdc.Scd.Web.Client/src/Common/Components/Accordion.tsx
@@ -0,0 +1,36 @@
+﻿import * as React from "react";
+
+export interface AccordionProps {
+    title: React.ReactNode;
+}
+
+export class Accordion extends React.Component<AccordionProps, any> {
+
+    private header: HTMLElement;
+
+    private body: HTMLElement;
+
+    public render() {
+        return (
+            <div className="accordion-block">
+                <div ref={x => this.header = x} className="accordion" onClick={x => this.onHeaderClick()}>
+                    {this.props.title}
+                </div>
+                <div ref={x => this.body = x} className="accordion-panel">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+
+    private onHeaderClick() {
+        this.header.classList.toggle("accordion-active");
+
+        let body = this.body;
+        if (body.style.maxHeight) {
+            body.style.maxHeight = null;
+        } else {
+            body.style.maxHeight = body.scrollHeight + "px";
+        }
+    }
+}
--- Gdc.Scd.Web.Client/src/CostApproval/Components/ApprovalBundleItemComponent.tsx
+++ Gdc.Scd.Web.Client/src/CostApproval/Components/ApprovalBundleItemComponent.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ApprovalBundle } from "../States/ApprovalBundle";
 import { Panel } from "@extjs/ext-react";
 import { ApprovalValuesContainerComponent } from "./ApprovalValuesContainerComponent";
+import { Accordion } from "../../Common/Components/Accordion";
 
 Ext.require('Ext.panel.Collapser');
 
@@ -27,63 +28,109 @@ export class ApprovalBundleItemComponent extends React.Component<ApprovalBundleI
         const { bundle, children, isCheckColumnsVisible } = this.props;
         const { id, costBlock } = bundle;
 
+
         return (
-            <Panel 
-                title={this.getTitle()} 
-                layout="fit"
-                shadow
-                collapsed={true}
-                collapsible={{
-                    direction: 'top',
-                    dynamic: true
-                }}
-                onExpand={this.onPanelExpanded}
-                margin="5px 10px 5px 10px"
+            <Accordion
+                title={this.getTitle()}
             >
-                {
-                    this.state.isFirstExpand &&
-                    <ApprovalValuesContainerComponent approvalBundle={bundle} isCheckColumnsVisible={isCheckColumnsVisible}>
-                        {children}
-                    </ApprovalValuesContainerComponent>
-                }
-            </Panel>
+                <ApprovalValuesContainerComponent approvalBundle={bundle} isCheckColumnsVisible={isCheckColumnsVisible}>
+                    {children}
+                </ApprovalValuesContainerComponent>
+            </Accordion>
         );
+
+        //return (
+        //    <Panel 
+        //        title={this.getTitle()} 
+        //        layout="fit"
+        //        shadow
+        //        collapsed={true}
+        //        collapsible={{
+        //            direction: 'top',
+        //            dynamic: true
+        //        }}
+        //        onExpand={this.onPanelExpanded}
+        //        margin="5px 10px 5px 10px"
+        //    >
+        //        {
+        //            this.state.isFirstExpand &&
+        //            <ApprovalValuesContainerComponent approvalBundle={bundle} isCheckColumnsVisible={isCheckColumnsVisible}>
+        //                {children}
+        //            </ApprovalValuesContainerComponent>
+        //        }
+        //    </Panel>
+        //);
     }
 
     private onPanelExpanded = () => {
         if (!this.state.isFirstExpand) {
-            this.setState({isFirstExpand: true})
+            this.setState({ isFirstExpand: true })
         }
     }
 
+    //private getTitle() {
+    //    const {
+    //        editDate,
+    //        editUser,
+    //        regionInput,
+    //        costElement,
+    //        editItemCount,
+    //        inputLevel,
+    //        costBlock
+    //    } = this.props.bundle;
+
+    //    const date = new Date(editDate);
+
+    //    return `
+    //        <div style="float: left; padding-right: 50px;">
+    //            <div>Date: ${date.toDateString()}</div>
+    //            <div>User: ${editUser.name}</div>
+    //            ${
+    //        regionInput
+    //            ? `<div>Region: ${regionInput.name}</div>`
+    //            : ''
+    //        }
+    //        </div>
+    //        <div style="float: right;">
+    //            <div>Cost block: ${costBlock.name}</div>
+    //            <div>Cost element: ${costElement.name}</div>
+    //            <div>${editItemCount} ${inputLevel.name}</div>
+    //        </div>
+    //    `;
+    //}
+
     private getTitle() {
-        const { 
-            editDate, 
-            editUser, 
-            regionInput, 
-            costElement, 
-            editItemCount, 
+        const {
+            editDate,
+            editUser,
+            regionInput,
+            costElement,
+            editItemCount,
             inputLevel,
-            costBlock 
+            costBlock
         } = this.props.bundle;
 
         const date = new Date(editDate);
 
-        return `
-            <div style="float: left; padding-right: 50px;">
-                <div>Date: ${date.toDateString()}</div>
-                <div>User: ${editUser.name}</div>
-                ${
-                    regionInput 
-                        ? `<div>Region: ${regionInput.name}</div>` 
-                        : ''
-                }
+        let region = null;
+
+        if (regionInput) {
+            region = <div>Region: {regionInput.name}</div>;
+        }
+
+        return (
+            <div>
+                <div>
+                    <div>Date: {date.toDateString()}</div>
+                    <div>User: {editUser.name}</div>
+                    {region}
+                </div>
+                <div>
+                    <div>Cost block: {costBlock.name}</div>
+                    <div>Cost element: {costElement.name}</div>
+                    <div>{editItemCount} {inputLevel.name}</div>
+                </div>
             </div>
-            <div style="float: right;">
-                <div>Cost block: ${costBlock.name}</div>
-                <div>Cost element: ${costElement.name}</div>
-                <div>${editItemCount} ${inputLevel.name}</div>
-            </div>
-        `;
+        );
     }
 }
