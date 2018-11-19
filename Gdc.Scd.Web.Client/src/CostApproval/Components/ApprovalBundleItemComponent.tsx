30c32726c06fc61ed05947a3634606a2160a7cf1
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-11-19 10:35:00 +0300

    fix styles

--- Gdc.Scd.Web.Client/src/CostApproval/Components/ApprovalBundleItemComponent.tsx
+++ Gdc.Scd.Web.Client/src/CostApproval/Components/ApprovalBundleItemComponent.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
-import { ApprovalBundle } from "../States/ApprovalBundle";
-import { Panel } from "@extjs/ext-react";
-import { ApprovalValuesContainerComponent } from "./ApprovalValuesContainerComponent";
 import { Accordion } from "../../Common/Components/Accordion";
-
-Ext.require('Ext.panel.Collapser');
+import { ApprovalBundle } from "../States/ApprovalBundle";
+import { ApprovalValuesContainerComponent } from "./ApprovalValuesContainerComponent";
 
 export interface ApprovalBundleItemProps {
     bundle: ApprovalBundle
@@ -26,78 +23,15 @@ export class ApprovalBundleItemComponent extends React.Component<ApprovalBundleI
 
     public render() {
         const { bundle, children, isCheckColumnsVisible } = this.props;
-        const { id, costBlock } = bundle;
-
 
         return (
-            <Accordion
-                title={this.getTitle()}
-            >
+            <Accordion title={this.getTitle()}>
                 <ApprovalValuesContainerComponent approvalBundle={bundle} isCheckColumnsVisible={isCheckColumnsVisible}>
                     {children}
                 </ApprovalValuesContainerComponent>
             </Accordion>
         );
-
-        //return (
-        //    <Panel 
-        //        title={this.getTitle()} 
-        //        layout="fit"
-        //        shadow
-        //        collapsed={true}
-        //        collapsible={{
-        //            direction: 'top',
-        //            dynamic: true
-        //        }}
-        //        onExpand={this.onPanelExpanded}
-        //        margin="5px 10px 5px 10px"
-        //    >
-        //        {
-        //            this.state.isFirstExpand &&
-        //            <ApprovalValuesContainerComponent approvalBundle={bundle} isCheckColumnsVisible={isCheckColumnsVisible}>
-        //                {children}
-        //            </ApprovalValuesContainerComponent>
-        //        }
-        //    </Panel>
-        //);
     }
-
-    private onPanelExpanded = () => {
-        if (!this.state.isFirstExpand) {
-            this.setState({ isFirstExpand: true })
-        }
-    }
-
-    //private getTitle() {
-    //    const {
-    //        editDate,
-    //        editUser,
-    //        regionInput,
-    //        costElement,
-    //        editItemCount,
-    //        inputLevel,
-    //        costBlock
-    //    } = this.props.bundle;
-
-    //    const date = new Date(editDate);
-
-    //    return `
-    //        <div style="float: left; padding-right: 50px;">
-    //            <div>Date: ${date.toDateString()}</div>
-    //            <div>User: ${editUser.name}</div>
-    //            ${
-    //        regionInput
-    //            ? `<div>Region: ${regionInput.name}</div>`
-    //            : ''
-    //        }
-    //        </div>
-    //        <div style="float: right;">
-    //            <div>Cost block: ${costBlock.name}</div>
-    //            <div>Cost element: ${costElement.name}</div>
-    //            <div>${editItemCount} ${inputLevel.name}</div>
-    //        </div>
-    //    `;
-    //}
 
     private getTitle() {
         const {
@@ -119,7 +53,7 @@ export class ApprovalBundleItemComponent extends React.Component<ApprovalBundleI
         }
 
         return (
-            <div>
+            <div className="approval-panel-title">
                 <div>
                     <div>Date: {date.toDateString()}</div>
                     <div>User: {editUser.name}</div>
