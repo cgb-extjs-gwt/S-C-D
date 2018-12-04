e229918d19a290c9798c48dff2914a1acb98dfef
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2018-12-04 14:51:37 +0300

    Table view new columns

--- Gdc.Scd.Web.Client/src/TableView/Components/TableViewContainer.ts
+++ Gdc.Scd.Web.Client/src/TableView/Components/TableViewContainer.ts
@@ -1,223 +1,35 @@
-import { connect, connectAdvanced } from "react-redux";
+import { connect } from "react-redux";
+import { TableViewProps, TableView } from "./TableView";
 import { CommonState } from "../../Layout/States/AppStates";
-import { ColumnInfo, ColumnType } from "../../Common/States/ColumnInfo";
-import { findMeta } from "../../Common/Helpers/MetaHelper";
-import { NamedId } from "../../Common/States/CommonStates";
-import { CostBlockMeta, FieldType, CostElementMeta, CostMetaData } from "../../Common/States/CostMetaStates";
-import { buildGetRecordsUrl, getTableViewInfo, updateRecords } from "../Services/TableViewService";
-import { handleRequest } from "../../Common/Helpers/RequestHelper";
-import { CommonAction } from "../../Common/Actions/CommonActions";
-import { loadTableViewInfo, editRecord, resetChanges } from "../Actions/TableViewActions";
+import { CostElementIdentifier } from "../../Common/States/CostElementIdentifier";
 import { TableViewInfo } from "../States/TableViewState";
+import { Model } from "../../Common/States/ExtStates";
 import { TableViewRecord } from "../States/TableViewRecord";
-import { FieldInfo } from "../../Common/States/FieldInfo";
-import { Dispatch } from "redux";
-import { StoreOperation, Model } from "../../Common/States/ExtStates";
-import { isEqualCoordinates } from "../Helpers/TableViewHelper";
-import { TableView, TableViewProps, TableViewActions } from "./TableView";
+import { buildGetHistoryUrl } from "../Services/TableViewService";
 
-const mapToColumnInfo = (
-    fieldIfnos: FieldInfo[],
-    meta: CostMetaData,
-    costBlockCache: Map<string, CostBlockMeta>,
-    mapFn: (costBlock: CostBlockMeta, fieldInfo: FieldInfo) => ColumnInfo<TableViewRecord>
-) => fieldIfnos.map(fieldInfo => {
-    let costBlockMeta = costBlockCache.get(fieldInfo.metaId);
-    if (!costBlockMeta) {
-        costBlockMeta = findMeta(meta.costBlocks, fieldInfo.metaId);
+const buildHistotyDataLoadUrl = (tableViewInfo: TableViewInfo, [selection]: Model<TableViewRecord>[], selectedDataIndex: string) => {
+    const costElementField =
+        tableViewInfo.recordInfo.data.find(fieldInfo => fieldInfo.dataIndex == selectedDataIndex);
 
-        costBlockCache.set(fieldInfo.metaId, costBlockMeta);
-    }
-    
-    return mapFn(costBlockMeta, fieldInfo);
-}) 
-
-const buildColumn = (item: NamedId, fieldInfo: FieldInfo) => ({
-    title: item.name,
-    dataIndex: fieldInfo.dataIndex,
-})
-
-const buildCoordinateColumn = (costBlock: CostBlockMeta, fieldInfo: FieldInfo) => { 
-    let item: NamedId;
-
-    for (const { dependency, inputLevels } of costBlock.costElements) {
-        const items = dependency ? [dependency, ...inputLevels] : inputLevels;
-
-        item = items.find(x => x.id == fieldInfo.fieldName);
-
-        if (item) {
-            break;    
-        }
-    }
-
-    return <ColumnInfo<TableViewRecord>>{
-        ...buildColumn(item, fieldInfo),
-        type: ColumnType.Text,
-        mappingFn: (record: TableViewRecord) => record.coordinates[fieldInfo.dataIndex].name
+    const costElementId: CostElementIdentifier = {
+        applicationId: costElementField.schemaId,
+        costBlockId: costElementField.metaId,
+        costElementId: costElementField.fieldName
     };
-}
 
-const buildCostElementColumn = (costBlock: CostBlockMeta, fieldInfo: FieldInfo, state: TableViewInfo) => {
-    let type: ColumnType;
-    let referenceItems: Map<string, NamedId>;
+    const coordinates = {};
 
-    const costElement = findMeta(costBlock.costElements, fieldInfo.fieldName);
-    const fieldType = costElement.typeOptions ? costElement.typeOptions.Type : FieldType.Double;
-
-    switch (fieldType) {
-        case FieldType.Double:
-            type = ColumnType.Numeric;
-            break;
-
-        case FieldType.Flag:
-            type = ColumnType.CheckBox;
-            break;
-
-        case FieldType.Reference:
-            type = ColumnType.Reference;
-            referenceItems = new Map<string, NamedId>();
-
-            state.references[fieldInfo.dataIndex].forEach(item => referenceItems.set(item.id, item));
-            break;
+    for (const key of Object.keys(selection.data.coordinates)) {
+        coordinates[key] = selection.data.coordinates[key].id;
     }
 
-    return <ColumnInfo<TableViewRecord>>{
-        ...buildColumn(costElement, fieldInfo),
-        isEditable: true,
-        type,
-        referenceItems,
-        mappingFn: record => record.data[fieldInfo.dataIndex].value,
-        editMappingFn: (record, dataIndex) => record.data.data[dataIndex].value = record.get(dataIndex),
-        rendererFn: (value, record) => {
-            const dataIndex = buildCountDataIndex(fieldInfo.dataIndex);
-            const count = record.get(dataIndex);
-
-            return count == 1 ? value : `(${count} values)`;
-        }
-    };
+    return buildGetHistoryUrl(costElementId, coordinates);
 }
 
-const buildCountColumns = (costBlock: CostBlockMeta, fieldInfo: FieldInfo) => (<ColumnInfo<TableViewRecord>>{
-    isInvisible: true,
-    dataIndex: buildCountDataIndex(fieldInfo.dataIndex),
-    mappingFn: record => record.data[fieldInfo.dataIndex].count
-})
-
-const buildAdditionalColumns = (title, dataIndex) => {
-    return <ColumnInfo<TableViewRecord>>{
-        title: title,
-        dataIndex: dataIndex,
-        type: ColumnType.Text,
-        mappingFn: record => record.additionalData[dataIndex]
-    }
-}
-
-    
-
-const buildCountDataIndex = (dataIndex: string) => `${dataIndex}_Count`
-
-const buildProps = (state: CommonState) => {
-    let readUrl: string;
-    let updateUrl: string;
-    
-    const columns = [];
-    const filterDataIndexes = [];
-    const tableViewInfo = state.pages.tableView.info;
-    const meta = state.app.appMetaData;
-
-    if (tableViewInfo && meta) {
-        readUrl = buildGetRecordsUrl();
-
-        const costBlockCache = new Map<string, CostBlockMeta>();
-        const coordinateColumns = mapToColumnInfo(tableViewInfo.recordInfo.coordinates, meta, costBlockCache, buildCoordinateColumn);
-
-        const wgAdditionalColumns = [
-            buildAdditionalColumns("WG Full name", "Wg.Description"),
-            buildAdditionalColumns("PLA", "Wg.PLA"),
-            buildAdditionalColumns("Responsible Person", "Wg.ResponsiblePerson")
-        ]
-
-        const costElementColumns = mapToColumnInfo(
-            tableViewInfo.recordInfo.data, 
-            meta, 
-            costBlockCache, 
-            (costBlock, fieldInfo) => buildCostElementColumn(costBlock, fieldInfo, tableViewInfo));
-        
-        const countColumns = mapToColumnInfo(tableViewInfo.recordInfo.data, meta, costBlockCache, buildCountColumns);
-
-        columns.push(...countColumns, ...coordinateColumns, ...wgAdditionalColumns, ...costElementColumns);
-
-        coordinateColumns.forEach(column => filterDataIndexes.push(column.dataIndex));
-    }
-
-    return <TableViewProps<TableViewRecord>>{
-        columns,
-        filterDataIndexes,
-        apiUrls: {
-            read: readUrl
-        }
-    };
-}
-
-const buildActions = (state: CommonState, dispatch: Dispatch) => { 
-    const buildSaveFn = (isApproving: boolean) => () =>  handleRequest(
-        updateRecords(state.pages.tableView.editedRecords, isApproving).then(
-            () => dispatch(resetChanges())
-        )
-    );
-
-    return <TableViewActions<TableViewRecord>>{
-        init: () => !state.pages.tableView.info && handleRequest(
-            getTableViewInfo().then(
-                tableViewInfo => dispatch(loadTableViewInfo(tableViewInfo))
-            )
-        ),
-        onUpdateRecord: (store, record, operation, modifiedFieldNames) => {
-            switch (operation) {
-                case StoreOperation.Edit:
-                    const [dataIndex] = modifiedFieldNames;
-                    const tableViewRecord = record.data;
-                    const countDataIndex = buildCountDataIndex(dataIndex);
-                    if (dataIndex in record.data.coordinates || dataIndex in record.data.additionalData) {
-                        record.reject();
-                    }
-                    else
-                    if (record.get(countDataIndex) == 0) {
-                        record.data[countDataIndex] = 1;
-                    }
-                    break;
-            }
-        },
-        onUpdateRecordSet: (records, operation, dataIndex) => {
-            if (operation == StoreOperation.Edit) {
-                const tableViewRecords = records.map(rec => rec.data);
-
-                dispatch(editRecord(tableViewRecords, dataIndex));
-            }
-        },
-        onSave: buildSaveFn(false),
-        onApprove: buildSaveFn(true),
-        onCancel: () => dispatch(resetChanges()),
-        onLoadData: (store, records) => {
-            const editRecords = state.pages.tableView.editedRecords;
-            if (editRecords && editRecords.length > 0) {
-                for (const editRecord of editRecords) {
-                    const record = records.find(item => isEqualCoordinates(item.data, editRecord));
-
-                    if (record) {
-                        Object.keys(editRecord.data).forEach(key => {
-                            record.set(key, editRecord.data[key].value);
-                        });
-                    }
-                }
-            }
-        },
-    }
-}
-
-export const TableViewContainer = connectAdvanced<CommonState, TableViewProps, any>(
-    dispatch => state => ({
-        ...buildProps(state),
-        ...buildActions(state, dispatch)
+export const TableViewContainer = connect<TableViewProps, {}, {}, CommonState>(
+    ({ pages: { tableView } }) => ({
+        buildHistotyDataLoadUrl: tableView.info
+            ? (selection, selectedDataIndex) => buildHistotyDataLoadUrl(tableView.info, selection, selectedDataIndex)
+            : () => ''
     })
 )(TableView)
