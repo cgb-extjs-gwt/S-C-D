62b68bd5c873a60e3eae4b4aa9e1eea2f21e12ad
Gasfar Mukhametdinov <Gasfar.Mukhametdinov.GDC@ts.fujitsu.com>
2018-12-12 18:15:34 +0300

    Approve and reject messages now sending after approval and rejection

--- Gdc.Scd.BusinessLogicLayer/Impl/EmailService.cs
+++ Gdc.Scd.BusinessLogicLayer/Impl/EmailService.cs
@@ -1,13 +1,45 @@
-﻿using Gdc.Scd.BusinessLogicLayer.Interfaces;
+﻿using System.Net.Mail;
+using Gdc.Scd.BusinessLogicLayer.Interfaces;
 using Gdc.Scd.Core.Entities;
 
 namespace Gdc.Scd.BusinessLogicLayer.Impl
 {
     public class EmailService : IEmailService
     {
-        public void SendEmail(User recepient, string subject, string message)
+        public SmtpClient Client { get; set; }
+        public EmailService()
         {
-            //TODO: need to add behavior
+            Client = new SmtpClient
+            {
+                Port = 25,
+                Host = "mail.fsc.net",
+                Timeout = 10000,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+            };
+        }
+
+        public void SendApprovalMail(User recepient)
+        {
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("SCD_Admin@ts.fujitsu.com"),
+                IsBodyHtml = true,
+                Subject = "SCD: Approval granted",
+                Body = $"<b>Hello {recepient.Name},</br>Your values have been approved.</b>"
+            };
+            mailMessage.To.Add(recepient.Email);
+            Client.Send(mailMessage);
+        }
+        public void SendRejectedMail(User recepient, string rejectionText, string approverName)
+        {
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("SCD_Admin@ts.fujitsu.com"),
+                Subject = "SCD: Approval was not granted",
+                Body = $"<b>Hello {recepient.Name},</br>Your values are not approved.</br>Reason for rejection:</b> {rejectionText}. <b>Approver’s name: </b> {approverName}"
+            };
+            mailMessage.To.Add(recepient.Email);
+            Client.Send(mailMessage);
         }
     }
 }
