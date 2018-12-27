bd8a3096e4d67687783d860ed10b978d105b2d19
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2018-12-27 12:32:11 +0300

    fix script

--- Gdc.Scd.DataAccessLayer/DataBaseScripts/Calculation/calculation-sw.sql
+++ Gdc.Scd.DataAccessLayer/DataBaseScripts/Calculation/calculation-sw.sql
@@ -1,8 +1,4 @@
-﻿IF OBJECT_ID('SoftwareSolution.SwSpMaintenanceCostView', 'U') IS NOT NULL
-  DROP TABLE SoftwareSolution.SwSpMaintenanceCostView;
-go
-
-IF OBJECT_ID('SoftwareSolution.SwSpMaintenanceCostView', 'V') IS NOT NULL
+﻿IF OBJECT_ID('SoftwareSolution.SwSpMaintenanceCostView', 'V') IS NOT NULL
   DROP VIEW SoftwareSolution.SwSpMaintenanceCostView;
 go
 
