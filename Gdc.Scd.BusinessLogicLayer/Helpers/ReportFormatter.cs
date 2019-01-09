e151bb5097555762f102e65160f2d62582480f94
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-01-09 17:58:43 +0300

    add test

---
+++ Gdc.Scd.BusinessLogicLayer/Helpers/ReportFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Gdc.Scd.BusinessLogicLayer.Helpers
+{
+    public static class ReportFormatter
+    {
+        static CultureInfo culture = CultureInfo.InvariantCulture;
+
+        public static string Format4Decimals(double v)
+        {
+            return v == 0 ? "0" : v.ToString("#.####", culture);
+        }
+
+        public static string FormatEuro(double v)
+        {
+            return v.ToString("#.##") + " EUR";
+        }
+
+        public static string FormatPercent(double v)
+        {
+            return v.ToString("#.###") + "%";
+        }
+
+        public static string FormatYesNo(bool v)
+        {
+            return v ? "YES" : "NO";
+        }
+    }
+}
---
+++ Gdc.Scd.Tests.BusinessLogicLayer/Helpers/ReportFormatterUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using Gdc.Scd.BusinessLogicLayer.Helpers;
+using NUnit.Framework;
+
+namespace Gdc.Scd.Tests.BusinessLogicLayer.Helpers
+{
+    public class ReportFormatterUnitTest
+    {
+        ///Check double string formatter with 4 decimals
+        [TestCase(000, "0")]
+        [TestCase(1, "1")]
+        [TestCase(123.45689, "123.4569")]
+        [TestCase(10.3, "10.3")]
+        public void Format4Decimals_Test(double v, string expected)
+        {
+            Assert.AreEqual(expected, ReportFormatter.Format4Decimals(v));
+        }
+
+        ///Check double EUR string formatter
+        [TestCase(000, "0")]
+        [TestCase(1, "1")]
+        [TestCase(123.45689, "123.4569")]
+        [TestCase(10.3, "10.3")]
+        public void Format4Decimals_Test(double v, string expected)
+        {
+            Assert.AreEqual(expected, ReportFormatter.FormatEuro(v));
+        }
+
+    }
+}
