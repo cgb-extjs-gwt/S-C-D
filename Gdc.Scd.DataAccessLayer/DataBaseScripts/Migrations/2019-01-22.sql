4d413256abaa24a6487aee1d86ae8ae32dac7e98
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2019-01-23 20:05:37 +0300

    cd_cs again

--- Gdc.Scd.DataAccessLayer/DataBaseScripts/Migrations/2019-01-22.sql
+++ Gdc.Scd.DataAccessLayer/DataBaseScripts/Migrations/2019-01-22.sql
@@ -515,4 +515,64 @@ RETURN
 
     from Hardware.GetCostsFull(@approved, @cnt, @wg, @av, @dur, @reactiontime, @reactiontype, @loc, @pro, @lastid, @limit)
 )
+GO
+/****** Object:  UserDefinedFunction [Report].[GetServiceCostsBySla]    Script Date: 26.10.2018 15:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER FUNCTION [Report].[GetServiceCostsBySla]
+(
+    @cnt nvarchar(200),
+    @loc nvarchar(200),
+    @av nvarchar(200),
+    @reactiontime nvarchar(200),
+    @reactiontype nvarchar(200),
+    @wg nvarchar(200),   
+    @dur nvarchar(200)
+)
+RETURNS @tbl TABLE (
+    Country nvarchar(200),
+    ServiceTC float, 
+    ServiceTP float, 
+    ServiceTP1 float,
+    ServiceTP2 float,
+    ServiceTP3 float,
+    ServiceTP4 float,
+    ServiceTP5 float
+)
+AS
+BEGIN
+
+    declare @cntId bigint;
+    declare @locId bigint;
+    declare @avId bigint;
+    declare @reactiontimeId bigint;
+    declare @reactiontypeId bigint;
+    declare @wgId bigint;
+    declare @durId bigint;
+
+    select @cntId = id from InputAtoms.Country where UPPER(Name)= UPPER(@cnt);
+    select @locId = id from Dependencies.ServiceLocation where UPPER(Name) = UPPER(@loc);
+    select @avId = id from Dependencies.Availability where ExternalName like '%' + @av + '%';
+    select @reactiontimeId = id from Dependencies.ReactionTime where UPPER(Name)=UPPER(@reactiontime);
+    select @reactiontypeId = id from Dependencies.ReactionType where UPPER(Name)=UPPER(@reactiontype);
+    select @wgId = id from InputAtoms.Wg where UPPER(Name)=UPPER(@wg);
+    select @durId = id from Dependencies.Duration where UPPER(Name)=UPPER(@dur);
+
+    INSERT @tbl
+    select costs.Country,
+           costs.ServiceTC_Released as ServiceTC, 
+           costs.ServiceTP_Released as ServiceTP, 
+           costs.ServiceTP1,
+           costs.ServiceTP2,
+           costs.ServiceTP3,
+           costs.ServiceTP4,
+           costs.ServiceTP5
+     from Hardware.GetCostsFull(0, @cntId, @wgId, @avId, @durId, @reactiontimeId, @reactiontypeId, @locId, null, 0, -1) costs
+
+    return;
+
+END
+GO
 
