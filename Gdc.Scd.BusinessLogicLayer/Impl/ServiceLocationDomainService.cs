5032221fe12fef557c612572fc18b0e584fbbbb4
Evgenia Borisova <mailtoevgenia@gmail.com>
2019-01-28 14:27:28 +0300

    Change interfaces

---
+++ Gdc.Scd.BusinessLogicLayer/Impl/ServiceLocationDomainService.cs
@@ -0,0 +1,26 @@
+﻿using Gdc.Scd.BusinessLogicLayer.Interfaces;
+using Gdc.Scd.Core.Entities;
+using Gdc.Scd.Core.Interfaces;
+using Gdc.Scd.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.BusinessLogicLayer.Impl
+{
+    public class SortableDomainService<T> : DomainService<T>, 
+                                                    ISortableDomainService<T> where T : class, IIdentifiable, ISortable, new()
+    {
+        public SortableDomainService(IRepositorySet repositorySet)
+            : base(repositorySet)
+        {
+        }
+
+        public override IQueryable<T> GetAll()
+        {
+            return base.GetAll().OrderBy(entity => entity.Order);
+        }
+    }
+}
---
+++ Gdc.Scd.BusinessLogicLayer/Interfaces/ISortableDomainService.cs
@@ -0,0 +1,14 @@
+﻿using Gdc.Scd.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.BusinessLogicLayer.Interfaces
+{
+    public interface ISortableDomainService<T> where T : ISortable
+    {
+        IQueryable<T> GetAll();
+    }
+}
