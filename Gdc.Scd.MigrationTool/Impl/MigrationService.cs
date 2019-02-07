70f388d42df8ab05d64388ceae81790b21647448
Timur Rizvanov <Timur.Rizvanov.gdc@ts.fujitsu.com>
2019-02-07 13:26:59 +0300

    MigrationTool was added.

---
+++ Gdc.Scd.MigrationTool/Impl/MigrationService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Entities;
+using Gdc.Scd.MigrationTool.Interfaces;
+using Gdc.Scd.MigrationTool.Migrations;
+
+namespace Gdc.Scd.MigrationTool.Impl
+{
+    public class MigrationService : IMigrationService
+    {
+        private readonly IRepositorySet repositorySet;
+
+        private readonly IEnumerable<IMigrationAction> migrationActions;
+
+        public MigrationService(IRepositorySet repositorySet, IEnumerable<IMigrationAction> migrationActions)
+        {
+            this.repositorySet = repositorySet;
+            this.migrationActions = migrationActions;
+        }
+
+        public IEnumerable<IMigrationAction> GetMigrationActions()
+        {
+            var errorActionNames = 
+                this.migrationActions.Where(action => action.Number == 0 || string.IsNullOrWhiteSpace(action.Description))
+                                     .Select(action => action.GetType().Name)
+                                     .ToArray();
+            if (errorActionNames.Length > 0)
+            {
+                throw new Exception($"Migration actions must have number and description. Error migrations: {string.Join(",", errorActionNames)}");
+            }
+
+            errorActionNames = 
+                this.migrationActions.GroupBy(action => action.Number)
+                                     .Where(group => group.Count() > 1)
+                                     .Select(action => action.GetType().Name)
+                                     .ToArray();
+
+            if (errorActionNames.Length > 0)
+            {
+                throw new Exception($"Migration actions must have unique numbers. Error migrations: {string.Join(",", errorActionNames)}");
+            }
+
+            try
+            {
+                var migrations = this.repositorySet.GetRepository<Migration>().GetAll();
+                var executedNumbers = new HashSet<int>(migrations.Select(migration => migration.Number));
+
+                return this.migrationActions.Where(action => !executedNumbers.Contains(action.Number));
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Message == "Invalid object name 'Migration'.")
+                {
+                    // HACK: Return migration which create 'Migration' table.
+                    return this.migrationActions.Where(action => action is Migration_2019_02_07);
+                }
+
+                throw ex;
+            }
+        }
+
+        public void SaveMigrationAsExecuted(IMigrationAction migrationAction)
+        {
+            var migrationRepository = this.repositorySet.GetRepository<Migration>();
+
+            migrationRepository.Save(new Migration
+            {
+                Number = migrationAction.Number,
+                Description = migrationAction.Description,
+                ExecutionDate = DateTime.UtcNow
+            });
+
+            this.repositorySet.Sync();
+        }
+    }
+}
---
+++ Gdc.Scd.MigrationTool/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using Gdc.Scd.Core.Helpers;
+using Gdc.Scd.MigrationTool.Interfaces;
+using Ninject;
+
+namespace Gdc.Scd.MigrationTool
+{
+    class Program
+    {
+        static readonly StandardKernel kernel;
+
+        static readonly IMigrationService migrationService;
+
+        static Program()
+        {
+            NinjectExtensions.IsConsoleApplication = true;
+
+            kernel = CreateKernel();
+            migrationService = kernel.Get<IMigrationService>();
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var migrationActions = migrationService.GetMigrationActions().ToArray();
+
+                Console.WriteLine($"{migrationActions.Length} migrations for executing");
+                Console.WriteLine();
+
+                var defaultColor = Console.ForegroundColor;
+
+                foreach (var migrationAction in migrationActions)
+                {
+                    Console.WriteLine($"Executing migration:");
+                    Console.WriteLine($"Number: {migrationAction.Number}");
+                    Console.WriteLine($"Description: {migrationAction.Description}");
+
+                    try
+                    {
+                        migrationAction.Execute();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error during execution migration:");
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine();
+
+                        Console.ForegroundColor = defaultColor;
+                        Console.WriteLine("Executing migration was stoped");
+
+                        break;
+                    }
+
+                    migrationService.SaveMigrationAsExecuted(migrationAction);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Migration succefully executed");
+                    Console.ForegroundColor = defaultColor;
+                    Console.WriteLine();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            return new StandardKernel(
+                new Core.Module(), 
+                new DataAccessLayer.Module(),
+                new BusinessLogicLayer.Module(),
+                new MigrationTool.Module());
+        }
+    }
+}
