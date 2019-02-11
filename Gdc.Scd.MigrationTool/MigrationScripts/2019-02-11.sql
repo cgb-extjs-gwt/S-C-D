f8f40b8286e1fdf93771556a4e22b2acf5c0ec92
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2019-02-11 14:51:22 +0300

    Move migration to MigrationTool

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-02-11.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-02-11.sql
@@ -1,9 +1,9 @@
 USE [Scd_2]
 GO
 
-delete from [dbo].[RolePermission] where PermissionId=1 and RoleId=5
+DELETE FROM [dbo].[RolePermission] WHERE PermissionId=1 and RoleId=5
 GO
 
 ALTER TABLE [InputAtoms].[RoleCode]
      ADD Deactivated bit
-go
+GO
---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_11.cs
@@ -0,0 +1,33 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.DataAccessLayer.SqlBuilders.Helpers;
+using Gdc.Scd.MigrationTool.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_02_11 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 3;
+
+        public string Description => "Remove PRS PSM permissions for Input Cost Elements + Disabled column for RoleCode";
+
+        public Migration_2019_02_11(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            var queries = new List<SqlHelper>();
+            queries.AddRange(SqlFormatter.BuildFromFile(@"MigrationScripts\2019-02-11.sql"));
+            foreach (var query in queries)
+                this.repositorySet.ExecuteSql(query);
+        }
+    }
+}
