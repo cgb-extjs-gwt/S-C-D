e504440e7023583207ab9f1a93f9a8c52b3012cb
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2019-02-11 14:11:09 +0300

    deactivate role codes instead of delete

---
+++ Gdc.Scd.BusinessLogicLayer/Impl/RoleCodeService.cs
@@ -0,0 +1,63 @@
+﻿using Gdc.Scd.BusinessLogicLayer.Interfaces;
+using Gdc.Scd.Core.Entities;
+using Gdc.Scd.DataAccessLayer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.BusinessLogicLayer.Impl
+{
+    public class RoleCodeService : IRoleCodeService
+    {
+        private readonly IDomainService<Wg> wgService;
+        private readonly IDomainService<RoleCode> roleService;
+
+        private const string StillReferencedMessage = "Role code(s) cannot be deleted because it is still referenced by warranty group(s).";
+
+        public RoleCodeService(IDomainService<Wg> wgService, IDomainService<RoleCode> roleService)
+        {
+            this.wgService = wgService;
+            this.roleService = roleService;
+        }
+
+        public bool Deactivate(RoleCode roleCode)
+        {
+            if (!wgService.GetAll().Where(x => x.RoleCodeId == roleCode.Id).Any())
+            {
+                roleCode.Deactivated = true;
+                roleService.Save(roleCode);
+                return true;
+            }
+            else
+            {
+                throw new Exception(StillReferencedMessage);
+            }
+        }
+
+        public bool Deactivate(IEnumerable<RoleCode> roleCodes)
+        {
+            var roleCodeIds = roleCodes.Select(x => x.Id).ToList();
+
+            if (!wgService.GetAll().Where(x => x.RoleCodeId!=null && roleCodeIds.Contains(x.RoleCodeId ?? 0)).Any())
+            {
+                foreach(var roleCode in roleCodes)
+                {
+                    roleCode.Deactivated = true;
+                    roleService.Save(roleCode);
+                }
+                return true;
+            }
+            else
+            {
+                throw new Exception(StillReferencedMessage);
+            }
+        }
+
+        public Task<RoleCode[]> GetAllActive()
+        {
+            return roleService.GetAll().Where(x => !x.Deactivated).GetAsync();
+        }
+    }
+}
---
+++ Gdc.Scd.BusinessLogicLayer/Interfaces/IRoleCodeService.cs
@@ -0,0 +1,16 @@
+﻿using Gdc.Scd.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.BusinessLogicLayer.Interfaces
+{
+    public interface IRoleCodeService
+    {
+        bool Deactivate(RoleCode roleCode);
+        bool Deactivate(IEnumerable<RoleCode> roleCode);
+        Task<RoleCode[]> GetAllActive();
+    }
+}
