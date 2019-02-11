33f63a28de9900a85e839c35546f132e4715c91e
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-02-11 16:32:41 +0300

    fix modul and migration

--- Gdc.Scd.DataAccessLayer/DataBaseScripts/Migrations/2019-02-11-2.sql
+++ Gdc.Scd.DataAccessLayer/DataBaseScripts/Migrations/2019-02-11-2.sql
@@ -82,5 +82,119 @@ insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull
 
 GO
 
+IF OBJECT_ID('Report.HddRetentionCentral') IS NOT NULL
+  DROP FUNCTION Report.HddRetentionCentral;
+go 
+
+CREATE FUNCTION Report.HddRetentionCentral
+(
+    @wg bigint
+)
+RETURNS TABLE 
+AS
+RETURN (
+    select    wg.Name as Wg
+            , wg.Description as WgDescription
+            , hdd.TransferPrice
+            , hdd.DealerPrice as DealerPrice
+            , hdd.ListPrice as ListPrice
+    from Hardware.HddRetentionView hdd
+    join InputAtoms.WgSogView wg on wg.Id = hdd.WgId
+    where (@wg is null or wg.Id = @wg)
+)
+
+GO
+
+declare @reportId bigint = (select Id from Report.Report where upper(Name) = 'HDD-RETENTION-CENTRAL');
+declare @index int = 0;
+
+delete from Report.ReportColumn where ReportId = @reportId;
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Wg', 'Warranty Group', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'WgDescription', 'Warranty Group Name', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'TransferPrice', 'Transfer Price', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'DealerPrice', 'Dealer Price (Central Reference)', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'ListPrice', 'List Price (Central Reference)', 1, 1);
+
+set @index = 0;
+delete from Report.ReportFilter where ReportId = @reportId;
+
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, (select id from Report.ReportFilterType where UPPER(name) = 'WG'), 'wg', 'Warranty Group');
+
+GO
+
+IF OBJECT_ID('Report.HddRetentionByCountry') IS NOT NULL
+  DROP FUNCTION Report.HddRetentionByCountry;
+go 
+
+CREATE FUNCTION Report.HddRetentionByCountry
+(
+    @cnt bigint,
+    @wg bigint
+)
+RETURNS TABLE 
+AS
+RETURN (
+    SELECT c.CountryGroup
+         , c.Name as Country
+         , wg.Name as Wg
+         , wg.Description as WgDescription
+         , fsp.Name as Fsp
+         , fsp.Name as TopFsp
+
+         , hdd.TransferPrice
+         , hdd.DealerPrice
+         , hdd.ListPrice
+
+    from Fsp.HwHddFspCodeTranslation fsp
+    join InputAtoms.CountryView c on c.Id = fsp.CountryId
+    join InputAtoms.WgSogView wg on wg.id = fsp.WgId
+    left join Hardware.HddRetentionView hdd on hdd.WgId = fsp.WgId
+
+    where     (@cnt is null or fsp.CountryId = @cnt)
+          and (@wg is null or fsp.WgId = @wg)
+)
+
+go
+
+declare @reportId bigint = (select Id from Report.Report where upper(Name) = 'HDD-RETENTION-COUNTRY');
+declare @index int = 0;
+
+delete from Report.ReportColumn where ReportId = @reportId;
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'CountryGroup', 'Country Group', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Country', 'Country Name', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Wg', 'Warranty Group', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'WgDescription', 'Warranty Group Name', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'Fsp', 'Support Pack Code', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 1, 'TopFsp', 'TopUp Code', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'TransferPrice', 'Transfer Price', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'DealerPrice', 'Dealer Price (Central Reference)', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, 4, 'ListPrice', 'List Price (Central Reference)', 1, 1);
+
+set @index = 0;
+delete from Report.ReportFilter where ReportId = @reportId;
+
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, (select id from Report.ReportFilterType where UPPER(name) = 'USERCOUNTRY'), 'cnt', 'Country Name');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, (select id from Report.ReportFilterType where UPPER(name) = 'WG'), 'wg', 'Warranty Group');
+
+
 
 
