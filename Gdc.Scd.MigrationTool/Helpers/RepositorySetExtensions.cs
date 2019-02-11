57565bce6d6cd117872c5c411c3968db817f696c
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-02-11 16:50:48 +0300

    add migration class

---
+++ Gdc.Scd.MigrationTool/Helpers/RepositorySetExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.DataAccessLayer.SqlBuilders.Helpers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Gdc.Scd.MigrationTool
+{
+    public static class RepositorySetExtensions
+    {
+        public static void ExecuteFromFile(this IRepositorySet repositorySet, string fn)
+        {
+            fn = Path.Combine(@"MigrationScripts", fn);
+            ExecuteQueries(repositorySet, SqlFormatter.BuildFromFile(fn));
+        }
+
+        public static void ExecuteQueries(this IRepositorySet repositorySet, IEnumerable<SqlHelper> queries)
+        {
+            foreach (var query in queries)
+            {
+                repositorySet.ExecuteSql(query);
+            }
+        }
+    }
+}
---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_11.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_02_11 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 1;
+
+        public string Description => "Hdd retention schema change. Add List price/dealer price. Fix hdd retention reports";
+
+        public Migration_2019_02_11(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-02-11.sql");
+        }
+    }
+}
