b823f1b4e65f2f20db4c16ced535ee6c5bcf4b54
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-02-12 13:30:57 +0300

    fix migration number

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_11_3.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_11_3.cs
@@ -3,22 +3,22 @@ using Gdc.Scd.MigrationTool.Interfaces;
 
 namespace Gdc.Scd.MigrationTool.Migrations
 {
-    public class Migration_2019_02_11 : IMigrationAction
+    public class Migration_2019_02_11_3 : IMigrationAction
     {
         private readonly IRepositorySet repositorySet;
 
-        public int Number => 1;
+        public int Number => 3;
 
         public string Description => "Hdd retention schema change. Add List price/dealer price. Fix hdd retention reports";
 
-        public Migration_2019_02_11(IRepositorySet repositorySet)
+        public Migration_2019_02_11_3(IRepositorySet repositorySet)
         {
             this.repositorySet = repositorySet;
         }
 
         public void Execute()
         {
-            repositorySet.ExecuteFromFile("2019-02-11.sql");
+            repositorySet.ExecuteFromFile("2019-02-11_3.sql");
         }
     }
 }
