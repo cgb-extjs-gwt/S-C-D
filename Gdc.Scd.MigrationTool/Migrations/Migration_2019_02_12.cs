6ebb71e50c8af9910a036ccc2ee5ba1ae8f4cc0b
Evgenia Borisova <mailtoevgenia@gmail.com>
2019-02-12 12:38:03 +0300

    Fixing isApplicable functionality for AvailabilityFee

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_12.cs
@@ -0,0 +1,28 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.DataAccessLayer.SqlBuilders.Helpers;
+using Gdc.Scd.MigrationTool.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_02_12 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 3;
+
+        public string Description => "Fix IsApplicable under Availability Fee";
+
+        public void Execute()
+        {
+            var queries = new List<SqlHelper>();
+            queries.AddRange(SqlFormatter.BuildFromFile(@"MigrationScripts\Migration_2019_02_12.sql"));
+            foreach (var query in queries)
+                this.repositorySet.ExecuteSql(query);
+        }
+    }
+}
