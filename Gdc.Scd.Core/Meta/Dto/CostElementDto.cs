0997c789740c9a8b27403733adf406c0e1384a6a
Timur Rizvanov <Timur.Rizvanov.gdc@ts.fujitsu.com>
2019-02-14 17:28:15 +0300

    #114017 Hadding input level was implemented.

--- Gdc.Scd.Core/Meta/Dto/CostElementDto.cs
+++ Gdc.Scd.Core/Meta/Dto/CostElementDto.cs
@@ -1,9 +1,19 @@
-﻿using Gdc.Scd.Core.Meta.Entities;
+﻿using System.Collections.Generic;
+using Gdc.Scd.Core.Meta.Entities;
 
 namespace Gdc.Scd.Core.Meta.Dto
 {
     public class CostElementDto : BaseCostElementMeta<InputLevelDto>
     {
+        private IEnumerable<InputLevelDto> inputLevels;
+
         public UsingInfo UsingInfo { get; set; }
+
+        public override IEnumerable<InputLevelDto> InputLevels => this.inputLevels;
+
+        public void SetInputLevels(IEnumerable<InputLevelDto> inputLevels)
+        {
+            this.inputLevels = inputLevels;
+        }
     }
 }
