ca159be9f90ff3f2b56d2210e2ad05dbfc7720db
Evgenia Borisova <mailtoevgenia@gmail.com>
2019-02-15 17:08:25 +0300

    remove unused usings

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_15.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_15.cs
@@ -1,10 +1,5 @@
 ﻿using Gdc.Scd.DataAccessLayer.Interfaces;
 using Gdc.Scd.MigrationTool.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gdc.Scd.MigrationTool.Migrations
 {
