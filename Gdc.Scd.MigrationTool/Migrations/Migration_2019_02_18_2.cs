8132b24f776f155063ef8fa0660e5f7af414aab6
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-02-18 17:22:00 +0300

    fix locap report

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_18_2.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_18_2.cs
@@ -3,22 +3,22 @@ using Gdc.Scd.MigrationTool.Interfaces;
 
 namespace Gdc.Scd.MigrationTool.Migrations
 {
-    public class Migration_2019_02_18 : IMigrationAction
+    public class Migration_2019_02_18_2 : IMigrationAction
     {
         private readonly IRepositorySet repositorySet;
 
-        public int Number => 13;
+        public int Number => 14;
 
-        public string Description => "Fix software calculation Service Support Cost";
+        public string Description => "Fix LOCAP report, fix service TC";
 
-        public Migration_2019_02_18(IRepositorySet repositorySet)
+        public Migration_2019_02_18_2(IRepositorySet repositorySet)
         {
             this.repositorySet = repositorySet;
         }
 
         public void Execute()
         {
-            repositorySet.ExecuteFromFile("2019-02-18.sql");
+            repositorySet.ExecuteFromFile("2019-02-18-2.sql");
         }
     }
 }
