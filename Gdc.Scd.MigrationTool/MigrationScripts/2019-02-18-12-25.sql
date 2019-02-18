4511ee7a5d8f4ddfc6e0eeff52b73abe835b3f4f
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2019-02-18 15:11:24 +0300

    service cost function changes

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-02-18-12-25.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-02-18-12-25.sql
@@ -19,3 +19,59 @@ INSERT INTO [Report].[CdCsConfiguration] ([CountryId],[FileFolderUrl], [FileWebU
 INSERT INTO [Report].[CdCsConfiguration] ([CountryId],[FileFolderUrl], [FileWebUrl]) VALUES (82,'/02/sites/p/ServiceCostDatabase/CSWE/CD_CS_CalculationTool','http://emeia.fujitsu.local/02/sites/p/ServiceCostDatabase/CSWE')
 INSERT INTO [Report].[CdCsConfiguration] ([CountryId],[FileFolderUrl], [FileWebUrl]) VALUES (118,'/teams/cor/SCD/Latin%20America/CD_CS_CalculationTool','https://partners.ts.fujitsu.com/teams/cor/SCD/Latin%20America')
 INSERT INTO [Report].[CdCsConfiguration] ([CountryId],[FileFolderUrl], [FileWebUrl]) VALUES (156,'/02/sites/p/ServiceCostDatabase/CMEA/CD_CS_CalculationTool','http://emeia.fujitsu.local/02/sites/p/ServiceCostDatabase/CMEA')
+GO
+
+ALTER FUNCTION [Report].[GetServiceCostsBySla]
+(
+    @cnt nvarchar(200),
+    @loc nvarchar(200),
+    @av nvarchar(200),
+    @reactiontime nvarchar(200),
+    @reactiontype nvarchar(200),
+    @wg nvarchar(200),   
+    @dur nvarchar(200)
+)
+RETURNS @tbl TABLE (
+    Country nvarchar(200),
+    ServiceTC float, 
+    ServiceTP float, 
+    ServiceTP1 float,
+    ServiceTP2 float,
+    ServiceTP3 float,
+    ServiceTP4 float,
+    ServiceTP5 float
+)
+AS
+BEGIN
+
+    declare @cntId dbo.ListId;
+    declare @locId dbo.ListId;
+    declare @avId dbo.ListId;
+    declare @reactiontimeId dbo.ListId;
+    declare @reactiontypeId dbo.ListId;
+    declare @wgId dbo.ListId;
+    declare @durId dbo.ListId;
+	declare @proId dbo.ListId;
+
+    insert into @cntId select id from InputAtoms.Country where UPPER(Name)= UPPER(@cnt);
+    insert into @locId select  id from Dependencies.ServiceLocation where UPPER(Name) = UPPER(@loc);
+    insert into @avId select   id from Dependencies.Availability where ExternalName like '%' + @av + '%';
+    insert into @reactiontimeId select   id from Dependencies.ReactionTime where UPPER(Name)=UPPER(@reactiontime);
+    insert into @reactiontypeId select   id from Dependencies.ReactionType where UPPER(Name)=UPPER(@reactiontype);
+    insert into @wgId select id from InputAtoms.Wg where UPPER(Name)=UPPER(@wg);
+    insert into @durId select id from Dependencies.Duration where UPPER(Name)=UPPER(@dur);
+
+    INSERT @tbl
+    select costs.Country,
+           coalesce(costs.ServiceTCManual, costs.ServiceTC) as ServiceTC, 
+		   costs.ServiceTP_Released as ServiceTP, 
+           costs.ServiceTP1,
+           costs.ServiceTP2,
+           costs.ServiceTP3,
+           costs.ServiceTP4,
+           costs.ServiceTP5
+     from Hardware.GetCostsFull(1, @cntId, @wgId, @avId, @durId, @reactiontimeId, @reactiontypeId, @locId, @proId, 0, -1) costs
+
+    return;
+
+END
