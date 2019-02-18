e9616ea2cdcfe7a3203294b3faf5eb49f39d1524
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-02-18 11:30:28 +0300

    add migration

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_18.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_02_18 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 12;
+
+        public string Description => "Fix software calculation Service Support Cost";
+
+        public Migration_2019_02_18(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-02-18.sql");
+        }
+    }
+}
