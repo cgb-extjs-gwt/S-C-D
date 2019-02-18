3aabac391a7c827deb9a5e91e481d2d95000b0d4
Timur Rizvanov <Timur.Rizvanov.gdc@ts.fujitsu.com>
2019-02-18 17:04:39 +0300

    #114016 Cost import was fixed.

--- Gdc.Scd.DataAccessLayer/SqlBuilders/Helpers/SqlHelper.cs
+++ Gdc.Scd.DataAccessLayer/SqlBuilders/Helpers/SqlHelper.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Gdc.Scd.DataAccessLayer.Entities;
 using Gdc.Scd.DataAccessLayer.SqlBuilders.Entities;
-using Gdc.Scd.DataAccessLayer.SqlBuilders.Impl;
 using Gdc.Scd.DataAccessLayer.SqlBuilders.Interfaces;
 
 namespace Gdc.Scd.DataAccessLayer.SqlBuilders.Helpers
@@ -33,43 +31,11 @@ namespace Gdc.Scd.DataAccessLayer.SqlBuilders.Helpers
 
         public IEnumerable<CommandParameterInfo> GetParameters()
         {
-            var paramDictionary = new Dictionary<string, CommandParameterInfo>();
+            var context = new SqlBuilderContext();
 
-            foreach (var param in this.GetParameters(this.sqlBuilder))
-            {
-                if (paramDictionary.TryGetValue(param.Name, out var paramDict))
-                {
-                    if (!object.Equals(param.Value, paramDict.Value))
-                    {
-                        throw new Exception("There are two parameters that have the same name, but different values.");
-                    }
-                }
-                else
-                {
-                    paramDictionary.Add(param.Name, param);
-                }
-            }
+            this.sqlBuilder.Build(context);
 
-            return paramDictionary.Values;
-        }
-
-        private IEnumerable<CommandParameterInfo> GetParameters(ISqlBuilder sqlBuilder)
-        {
-            var paramBuilder = sqlBuilder as ParameterSqlBuilder;
-            if (paramBuilder == null)
-            {
-                foreach (var childBuilder in sqlBuilder.GetChildrenBuilders())
-                {
-                    foreach (var paramInfo in this.GetParameters(childBuilder))
-                    {
-                        yield return paramInfo;
-                    }
-                }
-            }
-            else
-            {
-                yield return paramBuilder.ParamInfo;
-            }
+            return context.GetParameters();
         }
     }
 }
