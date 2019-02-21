a029847db705ef2fa6fa83585489e52350c6a55f
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2019-02-21 17:20:47 +0300

    add migration class

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_21_2.cs
@@ -0,0 +1,29 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_02_21_2 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 18;
+
+        public string Description => "Some reports are now in local currency";
+
+        public Migration_2019_02_21_2(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-02-20-17-11.sql");
+        }
+    }
+}
