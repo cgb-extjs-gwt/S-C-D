fee1e0d678c3275c57c7a6e4d25ebbcfec1bb850
Timur Rizvanov <Timur.Rizvanov.gdc@ts.fujitsu.com>
2019-02-22 14:19:18 +0300

    #105720 Mark approved elements. Table View. Server side. Intermediate commit.

---
+++ Gdc.Scd.DataAccessLayer/Impl/CostBlockQueryBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gdc.Scd.DataAccessLayer.Entities;
+using Gdc.Scd.DataAccessLayer.SqlBuilders.Helpers;
+
+namespace Gdc.Scd.DataAccessLayer.Impl
+{
+    public class CostBlockQueryBuilder
+    {
+        public OrderBySqlHelper BuildSelectQuery(CostBlockSelectQueryData queryData)
+        {
+
+        }
+    }
+}
