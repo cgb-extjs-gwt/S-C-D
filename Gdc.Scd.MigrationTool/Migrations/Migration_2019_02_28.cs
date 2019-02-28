c5d2d3cc9335de6aafb9dc9aab73995575d926f2
Evgenia Borisova <mailtoevgenia@gmail.com>
2019-02-28 18:02:27 +0300

    Fixed migration number

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_28.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_02_28.cs
@@ -12,7 +12,7 @@ namespace Gdc.Scd.MigrationTool.Migrations
     {
         private readonly IRepositorySet repositorySet;
 
-        public int Number => 24;
+        public int Number => 26;
 
         public string Description => "Enable/Disable triggers Stored Procedures";
 
