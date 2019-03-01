56cfe1128232786ea97ccba6cbf3fcd2d65702d3
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-01 15:45:31 +0300

    batch update portfolio

--- Gdc.Scd.Web.Client/src/Portfolio/Services/PortfolioService.ts
+++ Gdc.Scd.Web.Client/src/Portfolio/Services/PortfolioService.ts
@@ -1,6 +1,7 @@
 import { post } from "../../Common/Services/Ajax";
 import { PortfolioEditModel } from "../Model/PortfolioEditModel";
 import { IPortfolioService } from "./IPortfolioService";
+import { DictFactory } from "../../Dict/Services/DictFactory";
 
 export class PortfolioService implements IPortfolioService {
     private controllerName: string;
@@ -21,24 +22,44 @@ export class PortfolioService implements IPortfolioService {
         return post(this.controllerName, 'denylocal', { countryId: cnt, items: ids });
     }
 
-    private postSequential(action: string, row: PortfolioEditModel) {
+    private postSequential(action: string, row: PortfolioEditModel): Promise<any> {
+        return this.getWgOrDefault(row.wgs).then(x => this.postSerial(action, x, row))
+    }
 
-        var p = Promise.resolve();
+    private postSerial(action: string, arr: string[], row: PortfolioEditModel): Promise<any> {
 
-        let wg = row.wgs;
+        //send batch update by 8wg only
 
-        if (wg == null || wg.length === 0) {
-            wg = 
+        let p = Promise.resolve();
+
+        let max = 8, k = 0, part = [];
+
+        for (let i = 0, len = arr.length; i < len; i++) {
+
+            if (k == max) {
+                let partCopy = part;
+                p = p.then(() => this.send(action, partCopy, row));
+                part = [];
+                k = 0;
+            }
+
+            part.push(arr[i]);
+            k++;
         }
 
-        for (let i = 0, len = wg.length; i < len; i++) {
+        return p.then(() => this.send(action, part, row));
+    }
 
+    private send(action: string, wg: string[], row: PortfolioEditModel): Promise<any> {
+        return post(this.controllerName, action, { ...row, wgs: wg });
+    }
+
+    private getWgOrDefault(wg: string[]): Promise<string[]> {
+        if (wg && wg.length > 0) {
+            return Promise.resolve(wg);
         }
-
-
-
-
-
-        return post(this.controllerName, action, row);
+        else {
+            return DictFactory.getDictService().getWG().then(x => x.map(y => y.id));
+        }
     }
 }
