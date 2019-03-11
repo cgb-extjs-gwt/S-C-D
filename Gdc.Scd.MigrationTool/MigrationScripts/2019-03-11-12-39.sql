05c0950dc67243d76c07a15097bd48fa4822814d
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-11 14:53:47 +0300

    fix migration

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-03-11-12-39.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-03-11-12-39.sql
@@ -382,4 +382,154 @@ BEGIN
 END
 go
 
+ALTER FUNCTION [Report].[SwServicePriceList]
+(
+    @sog bigint,
+    @digit bigint,
+    @av bigint,
+    @year bigint
+)
+RETURNS @tbl TABLE (
+      LicenseDescription nvarchar(max) NULL
+    , Sog nvarchar(max) NULL
+    , Fsp nvarchar(max) NULL
+    , ServiceDescription nvarchar(max) NULL
+    , ServiceShortDescription nvarchar(max) NULL
+      
+    , TP float NULL
+    , DealerPrice float NULL
+    , ListPrice float NULL
+)
+as
+begin
 
+    declare @digitList dbo.ListId; 
+
+    if @sog is not null or @digit is not null
+    begin
+
+        insert into @digitList(id)
+        select Id
+        from InputAtoms.SwDigit 
+        where     (@sog is null   or SogId = @sog) 
+              and (@digit is null or Id = @digit);
+
+        if not exists(select * from @digitList) return;
+
+    end
+
+    declare @avList dbo.ListId; 
+    if @av is not null insert into @avList(id) values(@av);
+
+    declare @yearList dbo.ListId; 
+    if @year is not null insert into @yearList(id) values(@year);
+
+    insert into @tbl
+    select 
+              lic.Description as LicenseDescription
+            , sog.Name as Sog
+            , fsp.Name as Fsp
+
+            , fsp.ServiceDescription as ServiceDescription
+            , fsp.ShortDescription as ServiceShortDescription
+
+            , sw.TransferPrice as TP
+            , sw.DealerPrice as DealerPrice
+            , sw.MaintenanceListPrice as ListPrice
+
+    from SoftwareSolution.GetCosts(1, @digitList, @avList, @yearList, -1, -1) sw
+    join InputAtoms.SwDigit dig on dig.Id = sw.SwDigit
+    join InputAtoms.Sog sog on sog.id = sw.Sog
+
+    left join Fsp.SwFspCodeTranslation fsp on fsp.AvailabilityId = sw.Availability
+                                              and fsp.DurationId = sw.Year
+                                              and fsp.SwDigitId = sw.SwDigit
+
+    outer apply (
+
+        --get first existing row with valid description
+
+        SELECT top(1) lic.Description
+        FROM InputAtoms.SwLicense lic
+        WHERE lic.Description IS NOT NULL and exists (select * from InputAtoms.SwDigitLicense sdl where sdl.SwLicenseId = lic.Id and sdl.SwDigitId = dig.Id)
+    ) lic;
+
+    return
+end
+
+go
+
+ALTER FUNCTION [Report].[SwServicePriceListDetail]
+(
+    @sog bigint,
+    @digit bigint,
+    @av bigint,
+    @year bigint
+)
+RETURNS @tbl TABLE (
+      LicenseDescription nvarchar(max) NULL
+    , License nvarchar(max) NULL
+    , Sog nvarchar(max) NULL
+    , Fsp nvarchar(max) NULL
+    , ServiceDescription nvarchar(max) NULL
+    , ServiceShortDescription nvarchar(max) NULL
+      
+    , ServiceSupport float NULL
+    , Reinsurance float NULL
+      
+    , TP float NULL
+    , DealerPrice float NULL
+    , ListPrice float NULL
+)
+as
+begin
+    declare @digitList dbo.ListId; 
+
+    if @sog is not null or @digit is not null
+    begin
+
+        insert into @digitList(id)
+        select Id
+        from InputAtoms.SwDigit 
+        where     (@sog is null   or SogId = @sog) 
+              and (@digit is null or Id = @digit);
+
+        if not exists(select * from @digitList) return;
+
+    end
+
+    declare @avList dbo.ListId; 
+    if @av is not null insert into @avList(id) values(@av);
+
+    declare @yearList dbo.ListId; 
+    if @year is not null insert into @yearList(id) values(@year);
+
+    insert into @tbl
+    select    lic.Description as LicenseDescription
+            , lic.Name as License
+            , sog.Name as Sog
+            , fsp.Name as Fsp
+
+            , fsp.ServiceDescription as ServiceDescription
+            , fsp.ShortDescription as ServiceShortDescription
+
+            , sw.ServiceSupport as ServiceSupport
+            , sw.Reinsurance as Reinsurance
+
+            , sw.TransferPrice as TP
+            , sw.DealerPrice as DealerPrice
+            , sw.MaintenanceListPrice as ListPrice
+
+    from SoftwareSolution.GetCosts(1, @digitList, @avList, @yearList, -1, -1) sw
+    join InputAtoms.SwDigit dig on dig.Id = sw.SwDigit
+    join InputAtoms.Sog sog on sog.id = sw.Sog
+    left join InputAtoms.SwDigitLicense diglic on dig.Id = diglic.SwDigitId
+    left join InputAtoms.SwLicense lic on lic.Id = diglic.SwLicenseId
+    left join Fsp.SwFspCodeTranslation fsp on fsp.AvailabilityId = sw.Availability
+                                          and fsp.DurationId = sw.Year
+                                          and fsp.SwDigitId = sw.SwDigit;
+
+    return;
+end
+
+go
