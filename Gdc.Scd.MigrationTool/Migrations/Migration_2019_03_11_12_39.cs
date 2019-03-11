40332a435682534be7c62cc23b221c25103ca872
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-11 14:04:18 +0300

    add migrarion

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_11_12_39.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_03_11_12_39 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 33;
+
+        public string Description => "Add software parameter overview report";
+
+        public Migration_2019_03_11_12_39(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-03-11-12-39.sql");
+        }
+    }
+}
