96812abbb6f962a1c0d88230be06dd9e53befdac
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-12 12:32:32 +0300

    fix column heading

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_12_12_30.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_03_12_12_30 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 38;
+
+        public string Description => "Fix report sw overview column heading";
+
+        public Migration_2019_03_12_12_30(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-03-12-12-30.sql");
+        }
+    }
+}
