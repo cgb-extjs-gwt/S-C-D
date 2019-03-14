3bd475ecd89aaeea03ab3f1fff99029e98ce8040
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-14 14:23:37 +0300

    fix migration

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_13_18_00.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_13_18_00.cs
@@ -7,7 +7,7 @@ namespace Gdc.Scd.MigrationTool.Migrations
     {
         private readonly IRepositorySet repositorySet;
 
-        public int Number => 38;
+        public int Number => 39;
 
         public string Description => "Optimize calculation";
 
