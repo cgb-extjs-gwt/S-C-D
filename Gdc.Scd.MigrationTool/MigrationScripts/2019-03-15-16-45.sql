99653b530909619e70222ada34abc4c985ee5ab4
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-15 16:53:15 +0300

    add migration

---
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-03-15-16-45.sql
@@ -0,0 +1,20 @@
+﻿IF OBJECT_ID('History.PortfolioHistory', 'U') IS NOT NULL
+  DROP TABLE History.PortfolioHistory;
+go
+
+CREATE TABLE History.PortfolioHistory (
+    [Id] [bigint] IDENTITY(1,1) NOT NULL primary key,
+    [EditDate] [datetime2](7) NOT NULL,
+    [EditUserId] [bigint] NULL foreign key references [dbo].[User] ([Id]),
+    [Deny] bit NOT NULL,
+    [CountryId] [bigint] NULL foreign key references InputAtoms.Country(Id),
+    [RuleSet] nvarchar(max)
+)
+
+GO
+
+if not exists (select id from dbo.Role where name = 'Portfolio')
+begin
+    insert into dbo.[Role](Name, IsGlobal) values ('Portfolio', 1);
+    insert into dbo.RolePermission(PermissionId, RoleId) values((select id from dbo.Permission where Name = 'Portfolio'), (select id from dbo.Role where name = 'Portfolio'));
+end
---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_15_16_45.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_03_15_16_45 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 45;
+
+        public string Description => "Change hardware get cost, add sog";
+
+        public Migration_2019_03_15_16_45(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-03-15-16-45.sql");
+        }
+    }
+}
