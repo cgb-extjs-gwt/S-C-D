e4b1387fa16dce854b97ac55fbfb4f91cc501a15
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-15 10:48:38 +0300

    fix locap report, add multi select wg

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-03-15-10-45.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-03-15-10-45.sql
@@ -1,4 +1,190 @@
-﻿IF OBJECT_ID('Report.spLocapDetailed') IS NOT NULL
+﻿ALTER FUNCTION [Portfolio].[GetBySlaSog](
+    @cnt          bigint,
+    @wg           dbo.ListID readonly,
+    @av           bigint,
+    @dur          bigint,
+    @reactiontime bigint,
+    @reactiontype bigint,
+    @loc          bigint,
+    @pro          bigint
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+    with cte as (
+        select id
+        from InputAtoms.Wg 
+        where SogId in (
+                select wg.SogId from InputAtoms.Wg wg  where (not exists(select 1 from @wg) or exists(select 1 from @wg where id = wg.Id))
+            )
+            and IsSoftware = 0
+    )
+    select m.*
+    from Portfolio.LocalPortfolio m
+    join cte wg on wg.Id = m.WgId
+    where   m.CountryId = @cnt
+
+        AND (@av           is null or @av           = m.AvailabilityId    )
+        AND (@dur          is null or @dur          = m.DurationId        )
+        AND (@reactiontime is null or @reactiontime = m.ReactionTimeId    )
+        AND (@reactiontype is null or @reactiontype = m.ReactionTypeId    )
+        AND (@loc          is null or @loc          = m.ServiceLocationId )
+        AND (@pro          is null or @pro          = m.ProActiveSlaId    )
+)
+go
+
+IF OBJECT_ID('Report.spLocap') IS NOT NULL
+  DROP PROCEDURE Report.spLocap;
+go 
+
+CREATE PROCEDURE [Report].[spLocap]
+(
+    @cnt          bigint,
+    @wg           dbo.ListID readonly,
+    @av           bigint,
+    @dur          bigint,
+    @reactiontime bigint,
+    @reactiontype bigint,
+    @loc          bigint,
+    @pro          bigint,
+    @lastid       bigint,
+    @limit        int
+)
+AS
+BEGIN
+
+    declare @sla Portfolio.Sla;
+
+    insert into @sla 
+        select   -1
+                , Id
+                , CountryId
+                , WgId
+                , AvailabilityId
+                , DurationId
+                , ReactionTimeId
+                , ReactionTypeId
+                , ServiceLocationId
+                , ProActiveSlaId
+                , Sla
+                , SlaHash
+                , ReactionTime_Avalability
+                , ReactionTime_ReactionType
+                , ReactionTime_ReactionType_Avalability
+                , null
+                , null
+    from Portfolio.GetBySlaSog(@cnt, @wg, @av, @dur, @reactiontime, @reactiontype, @loc, @pro);
+
+    with cte as (
+        select m.* 
+        from Hardware.GetCostsSlaSog(1, @sla) m
+        where (not exists(select 1 from @wg) or exists(select 1 from @wg where id = m.WgId))
+    )
+    , cte2 as (
+        select  
+                ROW_NUMBER() over(ORDER BY (SELECT 1)) as rownum
+
+                , m.*
+                , fsp.Name as Fsp
+                , fsp.ServiceDescription as ServiceLevel
+
+        from cte m
+        left join Fsp.HwFspCodeTranslation fsp on fsp.SlaHash = m.SlaHash and fsp.Sla = m.Sla
+    )
+    select    m.Id
+            , m.Fsp
+            , m.WgDescription
+            , m.ServiceLevel
+
+            , m.ReactionTime
+            , m.Year as ServicePeriod
+            , m.Wg
+
+            , m.LocalServiceStandardWarranty * m.ExchangeRate as LocalServiceStandardWarranty
+            , m.ServiceTcSog * m.ExchangeRate as ServiceTC
+            , m.ServiceTpSog  * m.ExchangeRate as ServiceTP_Released
+            , cur.Name as Currency
+         
+            , m.Country
+            , m.Availability                       + ', ' +
+                  m.ReactionType                   + ', ' +
+                  m.ReactionTime                   + ', ' +
+                  cast(m.Year as nvarchar(1))      + ', ' +
+                  m.ServiceLocation                + ', ' +
+                  m.ProActiveSla as ServiceType
+
+            , null as PlausiCheck
+            , null as PortfolioType
+            , null as ReleaseCreated
+            , m.Sog
+
+    from cte2 m
+    join [References].Currency cur on cur.Id = m.CurrencyId
+
+    where (@limit is null) or (m.rownum > @lastid and m.rownum <= @lastid + @limit);
+
+END
+go
+
+declare @reportId bigint = (select Id from Report.Report where Name = 'Locap');
+declare @index int = 0;
+
+delete from Report.ReportColumn where ReportId = @reportId;
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'Fsp', 'Product_No', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'WgDescription', 'Warranty Group Name', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'ServiceLevel', 'Service Level', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'ReactionTime', 'Reaction Time', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'ServicePeriod', 'Service Period', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'Wg', 'WG', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('money'), 'LocalServiceStandardWarranty', 'Standard Warranty costs', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('money'), 'ServiceTC', 'Service TC', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('money'), 'ServiceTP_Released', 'Service TP (Released)', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'Country', 'Country Name', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'ServiceType', 'Service type', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'PlausiCheck', 'Plausi Check', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'PortfolioType', 'Portfolio Type', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'ReleaseCreated', 'Release created', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'Sog', 'SOG', 1, 1);
+
+set @index = 0;
+delete from Report.ReportFilter where ReportId = @reportId;
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('country', 0), 'cnt', 'Country Name');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('wg', 1), 'wg', 'Warranty Group');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('availability', 0), 'av', 'Availability');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('duration', 0), 'dur', 'Service period');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('reactiontime', 0), 'reactiontime', 'Reaction time');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('reactiontype', 0), 'reactiontype', 'Reaction type');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('servicelocation', 0), 'loc', 'Service location');
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('proactive', 0), 'pro', 'ProActive');
+
+GO
+
+
+IF OBJECT_ID('Report.spLocapDetailed') IS NOT NULL
   DROP PROCEDURE Report.spLocapDetailed;
 go 
 
