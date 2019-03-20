2d17e76d5830002c8d8657f6b85a9c33b03f13e7
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-20 15:38:03 +0300

    fix migration

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_20_15_25.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_20_15_25.cs
@@ -3,22 +3,22 @@ using Gdc.Scd.MigrationTool.Interfaces;
 
 namespace Gdc.Scd.MigrationTool.Migrations
 {
-    public class Migration_2019_03_15_16_45 : IMigrationAction
+    public class Migration_2019_03_20_15_25 : IMigrationAction
     {
         private readonly IRepositorySet repositorySet;
 
-        public int Number => 46;
+        public int Number => 48;
 
         public string Description => "Add portfolio history";
 
-        public Migration_2019_03_15_16_45(IRepositorySet repositorySet)
+        public Migration_2019_03_20_15_25(IRepositorySet repositorySet)
         {
             this.repositorySet = repositorySet;
         }
 
         public void Execute()
         {
-            repositorySet.ExecuteFromFile("2019-03-15-16-45.sql");
+            repositorySet.ExecuteFromFile("2019-03-20-15-25.sql");
         }
     }
 }
