78846f75701cd14438ba884204d2e3b2e8572284
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-20 09:05:20 +0300

    add history page

--- Gdc.Scd.Web.Client/src/Layout/Components/LayoutContainer.ts
+++ Gdc.Scd.Web.Client/src/Layout/Components/LayoutContainer.ts
@@ -9,7 +9,7 @@ import * as Permissions from "../../Common/Constants/Permissions";
 import { buildComponentUrl } from "../../Common/Services/Ajax";
 import { MenuItem } from "../../Common/States/ExtStates";
 import { CostEditorContainer } from "../../CostEditor/Components/CostEditorContainer";
-import { PortfolioEditView, PortfolioView } from "../../Portfolio/index";
+import { PortfolioEditView, PortfolioView, PortfolioHistoryView } from "../../Portfolio/index";
 import { ReportView } from "../../Report/index";
 import { ReportListView } from "../../Report/ReportListView";
 import { TableViewContainer } from "../../TableView/Components/TableViewContainer";
@@ -42,6 +42,7 @@ const buildRouteMenuItems = () => <RouteMenuItem[]>[
     { path: '/own-cost-approval', text: 'Own approve cost elements', iconCls: 'x-fa fa-check-square-o', component: OwnApproveLayoutContainer, isMenuItem: true, permission: Permissions.OWN_APPROVAL },
     { path: '/portfolio', text: 'Portfolio', iconCls: 'x-fa fa-suitcase', component: PortfolioView, isMenuItem: true, permission: Permissions.PORTFOLIO, exact: true },
     { path: '/portfolio/edit', component: PortfolioEditView, permission: Permissions.PORTFOLIO },
+    { path: '/portfolio/history', component: PortfolioHistoryView, permission: Permissions.PORTFOLIO },
     { path: '/report', text: 'Calculation Result', iconCls: 'x-fa fa-calculator', component: CalcResultViewContainer, isMenuItem: true, permission: Permissions.REPORT, exact: true },
     { path: '/report/all', text: 'Reports', iconCls: 'x-fa fa-bar-chart', component: ReportListView, isMenuItem: true, permission: Permissions.REPORT, exact: true },
     { path: '/report/:name', component: ReportView, exact: true, permission: Permissions.REPORT },
---
+++ Gdc.Scd.Web.Client/src/Portfolio/PortfolioHistoryView.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+
+export class PortfolioHistoryView extends React.Component<any, any> {
+
+    render() {
+        return <p>History here :)</p>
+    }
+
+}
