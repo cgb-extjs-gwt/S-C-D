738067157d2ebe9f667a01bf95282ecb76ddfe72
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-03-25 17:37:55 +0300

    add sog column to hdd retention report

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-03-25-12-55.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-03-25-12-55.sql
@@ -1702,3 +1702,61 @@ set @index = @index + 1;
 insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text, Value) values(@reportId, @index, Report.GetReportFilterTypeByName('proactive', 0), 'pro', 'ProActive', 1);
 
 GO
+
+IF OBJECT_ID('Report.HddRetentionCalcResult') IS NOT NULL
+  DROP FUNCTION Report.HddRetentionCalcResult;
+go 
+
+CREATE FUNCTION [Report].[HddRetentionCalcResult]
+	(	
+		@wg dbo.ListID readonly
+	)
+RETURNS TABLE 
+AS
+RETURN (
+    select h.Wg
+         , h.Sog
+         , h.TransferPrice
+         , h.ListPrice
+         , h.DealerDiscount
+         , h.DealerPrice
+		 , h.ChangeUserName + '[' + h.ChangeUserEmail + ']' as ChangeUser
+    from Hardware.HddRetentionView h
+	where Portfolio.IsListEmpty(@wg) = 1 or h.WgId in (select Id from @wg)
+)
+GO
+
+declare @reportId bigint = (select Id from Report.Report where upper(Name) = 'HDD-RETENTION-CALC-RESULT');
+declare @index int = 0;
+
+delete from Report.ReportColumn where ReportId = @reportId;
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'Wg', 'WG(Asset)', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'Sog', 'SOG(Asset)', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('euro'), 'TransferPrice', 'Transfer price', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('euro'), 'ListPrice', 'List price', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('percent'), 'DealerDiscount', 'Dealer discount in %', 1, 1);
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('euro'), 'DealerPrice', 'Dealer price', 1, 1);
+
+set @index = @index + 1;
+insert into Report.ReportColumn(ReportId, [Index], TypeId, Name, Text, AllowNull, Flex) values(@reportId, @index, Report.GetReportColumnTypeByName('text'), 'ChangeUser', 'Change user', 1, 1);
+
+--------------------
+
+set @index = 0;
+delete from Report.ReportFilter where ReportId = @reportId;
+
+set @index = @index + 1;
+insert into Report.ReportFilter(ReportId, [Index], TypeId, Name, Text) values(@reportId, @index, Report.GetReportFilterTypeByName('wg', 1), 'wg', 'Asset(WG)');
+
+GO
+
+
