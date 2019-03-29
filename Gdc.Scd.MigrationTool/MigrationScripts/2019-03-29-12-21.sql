c27d899bee2273ce81141c84a01a0b12e3e89349
Evgenia Borisova <evgenia.borisova.gdc@ts.fujitsu.com>
2019-03-29 20:38:47 +0300

    Fix Enable Trigger Procedure

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-03-29-12-21.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-03-29-12-21.sql
@@ -1,22 +1,22 @@
-USE [Scd_2]
+USE [SCD_2]
 GO
-
-IF OBJECT_ID('dbo.EnableAndRunTriggerOnTable') IS NOT NULL
-	DROP PROCEDURE [dbo].[EnableAndRunTriggerOnTable]
+/****** Object:  StoredProcedure [dbo].[EnableAndRunTriggerOnTable]    Script Date: 29.03.2019 11:55:46 ******/
+SET ANSI_NULLS ON
 GO
-
-CREATE PROCEDURE [dbo].[EnableAndRunTriggerOnTable] 
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[EnableAndRunTriggerOnTable] 
 	@tableName nvarchar(500)
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	--RUN ENABLE TRIGGER
 	DECLARE @cmd AS nvarchar(MAX)
 	SET @cmd = N'ENABLE TRIGGER ALL ON ' + @tableName
 	EXEC sp_executesql @cmd
 
-	--CALCULATE PRIMARY KEY COLUMN
+	
+
 	DECLARE @primaryKey AS nvarchar(max)
 	SELECT @primaryKey = column_name
 	FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
@@ -27,35 +27,14 @@ BEGIN
 				 '['+KU.TABLE_SCHEMA+'].['+KU.TABLE_NAME + ']'=@tableName
 	ORDER BY KU.TABLE_NAME, KU.ORDINAL_POSITION;
 
-	--CALCULATE COLUMN TO UPDATE
+
 	DECLARE @columnToUpdate AS nvarchar(max)
 	SELECT TOP(1) @columnToUpdate=[name] 
 	FROM syscolumns 
 	WHERE id=OBJECT_ID(@tableName) AND [name] != @primaryKey
 	
-	--UPDATE COLUMN TO RUN TRIGGER
 	SET @cmd = N'UPDATE ' + @tableName + 
 	             ' SET '+ @columnToUpdate + ' = ' + @columnToUpdate + 
 				 ' WHERE ' + @primaryKey + ' = (SELECT TOP(1) ' + @primaryKey + ' FROM ' + @tableName+')';
 	EXEC sp_executesql @cmd
 END
-
-GO
-
-IF OBJECT_ID('dbo.DisableTriggerOnTable') IS NOT NULL
-	DROP PROCEDURE [dbo].[DisableTriggerOnTable]
-GO
-
-CREATE PROCEDURE [dbo].[DisableTriggerOnTable] 
-	@tableName nvarchar(500) 
-AS
-BEGIN
-	SET NOCOUNT ON;
-	DECLARE @cmd AS nvarchar(MAX)
-	SET @cmd = N'DISABLE TRIGGER ALL ON ' + @tableName
-	
-	EXEC sp_executesql @cmd
-END
-
-GO
-
---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_03_29_12_32.cs
@@ -0,0 +1,29 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_03_29_12_32 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 63;
+
+        public string Description => "Enable Triggers use calculated PK column";
+
+        public Migration_2019_03_29_12_32(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-03-29-12-21.sql");
+        }
+    }
+}
