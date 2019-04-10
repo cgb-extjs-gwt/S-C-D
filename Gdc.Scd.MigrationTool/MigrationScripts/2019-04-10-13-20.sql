f920834939da133476ef3187fcada3f4960f9658
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-04-10 13:34:18 +0300

    fix report

--- Gdc.Scd.MigrationTool/MigrationScripts/2019-04-10-13-20.sql
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-04-10-13-20.sql
@@ -1,289 +1,4 @@
-﻿IF OBJECT_ID('dbo.HasScdAdminRole') IS NOT NULL
-  DROP FUNCTION dbo.HasScdAdminRole;
-go 
-
-IF OBJECT_ID('dbo.HasRole') IS NOT NULL
-  DROP FUNCTION dbo.HasRole;
-go 
-
-CREATE FUNCTION dbo.HasRole(@login nvarchar(255), @role nvarchar(255))
-RETURNS bit
-AS
-BEGIN
-
-    declare @result bit = 0;
-
-    if exists(select * from dbo.UserRole ur
-                where ur.UserId = (select id from dbo.[User] where Login = @login)
-                      and ur.RoleId = (select id from dbo.Role where UPPER(Name) = UPPER(@role)))
-       set @result = 1;
-   
-    RETURN @result;
-
-END
-
-go
-
-CREATE FUNCTION dbo.HasScdAdminRole(@login nvarchar(255))
-RETURNS bit
-AS
-BEGIN
-    return dbo.HasRole(@login, 'SCD ADMIN');
-END
-
-go
-
-IF OBJECT_ID('Report.GetCosts') IS NOT NULL
-  DROP FUNCTION Report.GetCosts;
-go 
-
-IF OBJECT_ID('Report.GetCostsFull') IS NOT NULL
-  DROP FUNCTION Report.GetCostsFull;
-go 
-
-IF OBJECT_ID('InputAtoms.CountryView', 'V') IS NOT NULL
-  DROP VIEW InputAtoms.CountryView;
-go
-
-CREATE VIEW InputAtoms.CountryView WITH SCHEMABINDING AS
-    select c.Id
-         , c.Name
-         , c.ISO3CountryCode
-         , c.IsMaster
-         , c.SAPCountryCode
-         , cg.Id as CountryGroupId
-         , cg.Name as CountryGroup
-         , cg.LUTCode
-         , cr.Id as ClusterRegionId
-         , cr.Name as ClusterRegion
-         , r.Id as RegionId
-         , r.Name as Region
-         , cur.Id as CurrencyId
-         , cur.Name as Currency
-    from InputAtoms.Country c
-    left join InputAtoms.CountryGroup cg on cg.Id = c.CountryGroupId
-    left join InputAtoms.Region r on r.Id = c.RegionId
-    left join InputAtoms.ClusterRegion cr on cr.Id = c.ClusterRegionId
-    left join [References].Currency cur on cur.Id = c.CurrencyId
-
-GO
-
-IF OBJECT_ID('Portfolio.IntToListID') IS NOT NULL
-  DROP FUNCTION Portfolio.IntToListID;
-go 
-
-CREATE FUNCTION Portfolio.IntToListID(@var bigint)
-RETURNS @tbl TABLE( id bigint NULL)
-AS
-BEGIN
-    insert @tbl(id) values (@var)
-RETURN
-END
-GO
-
-
-IF OBJECT_ID('[Report].[GetCosts]') IS NOT NULL
-    DROP FUNCTION [Report].[GetCosts]
-GO
-
-CREATE FUNCTION [Report].[GetCosts]
-(
-    @cnt bigint,
-    @wg bigint,
-    @av bigint,
-    @dur bigint,
-    @reactiontime bigint,
-    @reactiontype bigint,
-    @loc bigint,
-    @pro bigint
-)
-RETURNS @tbl TABLE (
-           Id                                bigint
-         , Fsp                               nvarchar(255)
-         , FspDescription                    nvarchar(255)
-         , CountryId                         bigint
-         , Country                           nvarchar(255)
-         , CurrencyId                        bigint
-         , ExchangeRate                      float
-         , WgId                              bigint
-         , Wg                                nvarchar(255)
-         , AvailabilityId                    bigint
-         , Availability                      nvarchar(255)
-         , DurationId                        bigint
-         , Duration                          nvarchar(255)
-         , Year                              int
-         , IsProlongation                    bit
-         , ReactionTimeId                    bigint
-         , ReactionTime                      nvarchar(255)
-         , ReactionTypeId                    bigint
-         , ReactionType                      nvarchar(255)
-         , ServiceLocationId                 bigint
-         , ServiceLocation                   nvarchar(255)
-         , ProActiveSlaId                    bigint
-         , ProActiveSla                      nvarchar(255)
-         , Sla                               nvarchar(255)
-         , SlaHash                           int
-         , StdWarranty                       float
-         , AvailabilityFee                   float
-         , TaxAndDutiesW                     float
-         , TaxAndDutiesOow                   float
-         , Reinsurance                       float
-         , ProActive                         float
-         , ServiceSupportCost                float
-         , MaterialW                         float
-         , MaterialOow                       float
-         , FieldServiceCost                  float
-         , Logistic                          float
-         , OtherDirect                       float
-         , LocalServiceStandardWarranty      float
-         , Credits                           float
-         , ServiceTC                         float
-         , ServiceTP                         float
-         , ServiceTC1                        float
-         , ServiceTC2                        float
-         , ServiceTC3                        float
-         , ServiceTC4                        float
-         , ServiceTC5                        float
-         , ServiceTC1P                       float
-         , ServiceTP1                        float
-         , ServiceTP2                        float
-         , ServiceTP3                        float
-         , ServiceTP4                        float
-         , ServiceTP5                        float
-         , ServiceTP1P                       float
-         , ListPrice                         float
-         , DealerDiscount                    float
-         , DealerPrice                       float
-         , ServiceTCManual                   float
-         , ServiceTPManual                   float
-         , ServiceTP_Released                float
-         , ChangeUserName                    nvarchar(255)
-         , ChangeUserEmail                   nvarchar(255)
-)
-AS
-begin
-
-    declare @cntTable dbo.ListId; insert into @cntTable(id) values(@cnt);
-
-    declare @wgTable dbo.ListId; if @wg is not null insert into @wgTable(id) values(@wg);
-
-    declare @avTable dbo.ListId; if @av is not null insert into @avTable(id) values(@av);
-
-    declare @durTable dbo.ListId; if @dur is not null insert into @durTable(id) values(@dur);
-
-    declare @rtimeTable dbo.ListId; if @reactiontime is not null insert into @rtimeTable(id) values(@reactiontime);
-
-    declare @rtypeTable dbo.ListId; if @reactiontype is not null insert into @rtypeTable(id) values(@reactiontype);
-
-    declare @locTable dbo.ListId; if @loc is not null insert into @locTable(id) values(@loc);
-
-    declare @proTable dbo.ListId; if @pro is not null insert into @proTable(id) values(@pro);
-    
-    insert into @tbl 
-    select 
-              c.Id                               
-            , fsp.Name as Fsp                              
-            , fsp.ServiceDescription as FspDescription                   
-            , c.CountryId                        
-            , c.Country                          
-            , c.CurrencyId                       
-            , c.ExchangeRate                     
-            , c.WgId                             
-            , c.Wg                               
-            , c.AvailabilityId                   
-            , c.Availability                     
-            , c.DurationId                       
-            , c.Duration                         
-            , c.Year                             
-            , c.IsProlongation                   
-            , c.ReactionTimeId                   
-            , c.ReactionTime                     
-            , c.ReactionTypeId                   
-            , c.ReactionType                     
-            , c.ServiceLocationId                
-            , c.ServiceLocation                  
-            , c.ProActiveSlaId                   
-            , c.ProActiveSla                     
-            , c.Sla                              
-            , c.SlaHash                          
-            , c.StdWarranty                      
-            , c.AvailabilityFee                  
-            , c.TaxAndDutiesW                    
-            , c.TaxAndDutiesOow                  
-            , c.Reinsurance                      
-            , c.ProActive                        
-            , c.ServiceSupportCost               
-            , c.MaterialW                        
-            , c.MaterialOow                      
-            , c.FieldServiceCost                 
-            , c.Logistic                         
-            , c.OtherDirect                      
-            , c.LocalServiceStandardWarranty     
-            , c.Credits                          
-            , c.ServiceTC                        
-            , c.ServiceTP                        
-            , c.ServiceTC1                       
-            , c.ServiceTC2                       
-            , c.ServiceTC3                       
-            , c.ServiceTC4                       
-            , c.ServiceTC5                       
-            , c.ServiceTC1P                      
-            , c.ServiceTP1                       
-            , c.ServiceTP2                       
-            , c.ServiceTP3                       
-            , c.ServiceTP4                       
-            , c.ServiceTP5                       
-            , c.ServiceTP1P                      
-            , c.ListPrice                        
-            , c.DealerDiscount                   
-            , c.DealerPrice                      
-            , c.ServiceTCManual                  
-            , c.ServiceTPManual                  
-            , c.ServiceTP_Released               
-            , c.ChangeUserName                   
-            , c.ChangeUserEmail                  
-    from Hardware.GetCosts(1, @cntTable, @wgTable, @avTable, @durTable, @rtimeTable, @rtypeTable, @locTable, @proTable, null, null) c
-    left join Fsp.HwFspCodeTranslation fsp on fsp.SlaHash = c.SlaHash and fsp.Sla = c.Sla    
-    return;
-end
-GO
-
-IF OBJECT_ID('Report.GetReportColumnTypeByName') IS NOT NULL
-  DROP FUNCTION Report.GetReportColumnTypeByName;
-go 
-
-CREATE FUNCTION Report.GetReportColumnTypeByName
-(
-    @name nvarchar(max)
-)
-RETURNS bigint 
-AS
-BEGIN
-	DECLARE @Id bigint
-    select @Id=id from Report.ReportColumnType where Name=@name
-	RETURN @Id
-END
-GO
-
-IF OBJECT_ID('Report.GetReportFilterTypeByName') IS NOT NULL
-  DROP FUNCTION Report.GetReportFilterTypeByName;
-go 
-
-CREATE FUNCTION Report.GetReportFilterTypeByName
-(
-    @name nvarchar(max),
-	@multi bit
-)
-RETURNS bigint 
-AS
-BEGIN
-	DECLARE @Id bigint
-    select @Id= id from Report.ReportFilterType where MultiSelect = @multi and name = @name
-	RETURN @Id
-END
-GO
-
-if OBJECT_ID('[Report].[GetParameterHw]') is not null
+﻿if OBJECT_ID('[Report].[GetParameterHw]') is not null
     drop function [Report].[GetParameterHw];
 GO
 
@@ -581,4 +296,197 @@ RETURN (
     from CostCte m
 )
 
+go
+
+ALTER FUNCTION [Report].[CalcParameterHw]
+(
+    @cnt bigint,
+    @wg bigint,
+    @av bigint,
+    @reactiontime bigint,
+    @reactiontype bigint,
+    @loc bigint,
+    @pro bigint
+)
+RETURNS TABLE 
+AS
+RETURN (
+    select    
+                m.Id
+              , m.Country
+              , m.WgDescription
+              , m.Wg
+              , m.SogDescription
+              , m.SCD_ServiceType
+              , m.Sla
+              , m.ServiceLocation
+              , m.ReactionTime
+              , m.ReactionType
+              , m.Availability
+
+             --FSP
+              , m.Fsp
+              , m.FspDescription
+
+              --cost blocks
+
+              , m.LabourCost as LabourCost
+              , m.TravelCost as TravelCost
+              , m.PerformanceRate as PerformanceRate
+              , m.TravelTime
+              , m.RepairTime
+              , m.OnsiteHourlyRate as OnsiteHourlyRate
+
+              , m.AvailabilityFee as AvailabilityFee
+      
+              , m.TaxAndDutiesW as TaxAndDutiesW
+
+              , m.MarkupOtherCost as MarkupOtherCost
+              , m.MarkupFactorOtherCost as MarkupFactorOtherCost
+
+              , m.MarkupFactorStandardWarranty as MarkupFactorStandardWarranty
+              , m.MarkupStandardWarranty as MarkupStandardWarranty
+      
+              , m.AFR1
+              , m.AFR2
+              , m.AFR3
+              , m.AFR4
+              , m.AFR5
+              , m.AFRP1
+
+              , m.[1stLevelSupportCosts]
+              , m.[2ndLevelSupportCosts]
+           
+              , m.ReinsuranceFlatfee1
+              , m.ReinsuranceFlatfee2
+              , m.ReinsuranceFlatfee3
+              , m.ReinsuranceFlatfee4
+              , m.ReinsuranceFlatfee5
+              , m.ReinsuranceFlatfeeP1
+              , m.ReinsuranceUpliftFactor_4h_24x7 as ReinsuranceUpliftFactor_4h_24x7
+              , m.ReinsuranceUpliftFactor_4h_9x5 as ReinsuranceUpliftFactor_4h_9x5
+              , m.ReinsuranceUpliftFactor_NBD_9x5 as ReinsuranceUpliftFactor_NBD_9x5
+
+              , m.MaterialCostWarranty
+              , m.MaterialCostOow
+
+              , m.Duration
+
+              , m.FieldServiceCost1
+              , m.FieldServiceCost2
+              , m.FieldServiceCost3
+              , m.FieldServiceCost4
+              , m.FieldServiceCost5
+            
+              , m.StandardHandling
+              , m.HighAvailabilityHandling
+              , m.StandardDelivery
+              , m.ExpressDelivery
+              , m.TaxiCourierDelivery
+              , m.ReturnDeliveryFactory 
+
+              , m.LogisticsHandling
+
+             , m.LogisticTransportcost
+
+            , m.Currency
+    from Report.GetParameterHw(1, @cnt, @wg, @av, @reactiontime, @reactiontype, @loc, @pro) m
+)
+go
+
+ALTER FUNCTION [Report].[CalcParameterHwNotApproved]
+(
+    @cnt bigint,
+    @wg bigint,
+    @av bigint,
+    @reactiontime bigint,
+    @reactiontype bigint,
+    @loc bigint,
+    @pro bigint
+)
+RETURNS TABLE 
+AS
+RETURN (
+    select    
+                m.Id
+              , m.Country
+              , m.WgDescription
+              , m.Wg
+              , m.SogDescription
+              , m.SCD_ServiceType
+              , m.Sla
+              , m.ServiceLocation
+              , m.ReactionTime
+              , m.ReactionType
+              , m.Availability
+              , m.Currency
+
+             --FSP
+              , m.Fsp
+              , m.FspDescription
+
+              --cost blocks
+
+              , m.LabourCost
+              , m.TravelCost
+              , m.PerformanceRate
+              , m.TravelTime
+              , m.RepairTime
+              , m.OnsiteHourlyRate
+
+              , m.AvailabilityFee
+      
+              , m.TaxAndDutiesW
+
+              , m.MarkupOtherCost
+              , m.MarkupFactorOtherCost
+
+              , m.MarkupFactorStandardWarranty
+              , m.MarkupStandardWarranty
+      
+              , m.AFR1
+              , m.AFR2
+              , m.AFR3
+              , m.AFR4
+              , m.AFR5
+              , m.AFRP1
+
+              , m.[1stLevelSupportCosts]
+              , m.[2ndLevelSupportCosts]
+           
+              , m.ReinsuranceFlatfee1
+              , m.ReinsuranceFlatfee2
+              , m.ReinsuranceFlatfee3
+              , m.ReinsuranceFlatfee4
+              , m.ReinsuranceFlatfee5
+              , m.ReinsuranceFlatfeeP1
+
+              , m.ReinsuranceUpliftFactor_4h_24x7
+              , m.ReinsuranceUpliftFactor_4h_9x5
+              , m.ReinsuranceUpliftFactor_NBD_9x5
+
+              , m.MaterialCostWarranty
+              , m.MaterialCostOow
+
+              , m.Duration
+
+              , m.FieldServiceCost1
+              , m.FieldServiceCost2
+              , m.FieldServiceCost3
+              , m.FieldServiceCost4
+              , m.FieldServiceCost5
+         
+              , m.StandardHandling
+              , m.HighAvailabilityHandling
+              , m.StandardDelivery
+              , m.ExpressDelivery
+              , m.TaxiCourierDelivery
+              , m.ReturnDeliveryFactory 
+         
+              , m.LogisticsHandling
+         
+              , m.LogisticTransportcost
+
+    from Report.GetParameterHw(0, @cnt, @wg, @av, @reactiontime, @reactiontype, @loc, @pro) m
+)
 go
---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_04_10_13_20.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_04_10_13_20 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 75;
+
+        public string Description => "Fix calc parameter hw report";
+
+        public Migration_2019_04_10_13_20(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-04-10-13-20.sql");
+        }
+    }
+}
