c5573d49869c9f41a8425a26e826da82b99990a1
Nikita Zotov <Nikita.Zotov.gdc@ts.fujitsu.com>
2019-04-18 18:49:09 +0300

    CD CS use released values + fix source files

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_04_18_17_43.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_04_18_17_43 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 81;
+
+        public string Description => "CD CS use released yearly costs";
+
+        public Migration_2019_04_18_17_43(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            repositorySet.ExecuteFromFile("2019-04-18-17-43.sql");
+        }
+    }
+}
