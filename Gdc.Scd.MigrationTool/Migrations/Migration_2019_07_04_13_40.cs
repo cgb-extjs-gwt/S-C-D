c168370f21e8df5e6a9892764933c49b4bafde48
Timur Rizvanov <Timur.Rizvanov.gdc@ts.fujitsu.com>
2019-07-04 15:23:26 +0300

    #132933 PSM release flag was added.

---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_07_04_13_40.cs
@@ -0,0 +1,25 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_07_04_13_40 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 113;
+
+        public string Description => "Adding PRS 'PsmRelease' in WG";
+
+        public Migration_2019_07_04_13_40(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            this.repositorySet.ExecuteSql(
+                "ALTER TABLE [InputAtoms].[Wg] ADD [PsmRelease] BIT NOT NULL DEFAULT(0)");
+        }
+    }
+}
