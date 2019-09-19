76a03c49fc41c33868fbb209c8551f508ad40047
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-09-19 10:21:41 +0300

    fix domain config, fix export to archive, add migration,

---
+++ Gdc.Scd.MigrationTool/MigrationScripts/2019-08-29-09-44.sql
@@ -0,0 +1,264 @@
+ALTER FUNCTION [Hardware].[GetCostsSlaSog](
+    @approved bit,
+    @cnt dbo.ListID readonly,
+    @wg dbo.ListID readonly,
+    @av dbo.ListID readonly,
+    @dur dbo.ListID readonly,
+    @reactiontime dbo.ListID readonly,
+    @reactiontype dbo.ListID readonly,
+    @loc dbo.ListID readonly,
+    @pro dbo.ListID readonly
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+    with cte as (
+        select    
+               m.Id
+
+             --SLA
+
+             , m.CountryId
+             , m.Country
+             , m.CurrencyId
+             , m.Currency
+             , m.ExchangeRate
+
+             , m.WgId
+             , m.Wg
+             , wg.Description as WgDescription
+             , m.SogId
+             , m.Sog
+
+             , m.AvailabilityId
+             , m.Availability
+             , m.DurationId
+             , m.Duration
+             , m.Year
+             , m.IsProlongation
+             , m.ReactionTimeId
+             , m.ReactionTime
+             , m.ReactionTypeId
+             , m.ReactionType
+             , m.ServiceLocationId
+             , m.ServiceLocation
+             , m.ProActiveSlaId
+             , m.ProActiveSla
+             , m.Sla
+             , m.SlaHash
+
+             , m.StdWarranty
+             , m.StdWarrantyLocation
+
+             --Cost
+
+             , m.AvailabilityFee
+             , m.TaxAndDutiesW
+             , m.TaxAndDutiesOow
+             , m.Reinsurance
+             , m.ProActive
+             , m.ServiceSupportCost
+             , m.MaterialW
+             , m.MaterialOow
+             , m.FieldServiceCost
+             , m.Logistic
+             , m.OtherDirect
+             , coalesce(m.LocalServiceStandardWarrantyManual, m.LocalServiceStandardWarranty) as LocalServiceStandardWarranty
+             , m.Credits
+
+             , ib.InstalledBaseCountryNorm
+
+             , (sum(m.ServiceTCResult * ib.InstalledBaseCountryNorm)                               over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_x_tc 
+             , (sum(case when m.ServiceTCResult <> 0 then ib.InstalledBaseCountryNorm end)          over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_by_tc
+
+             , (sum(m.ServiceTP_Released * ib.InstalledBaseCountryNorm)                      over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_x_tp_Released
+             , (sum(case when m.ServiceTP_Released <> 0 then ib.InstalledBaseCountryNorm end) over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_by_tp_Released
+
+             , (sum(m.ServiceTPResult * ib.InstalledBaseCountryNorm)                               over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_x_tp
+             , (sum(case when m.ServiceTPResult <> 0 then ib.InstalledBaseCountryNorm end)          over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_by_tp
+
+             , (sum(m.ProActive * ib.InstalledBaseCountryNorm)                               over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_x_pro
+             , (sum(case when m.ProActive <> 0 then ib.InstalledBaseCountryNorm end)         over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as sum_ib_by_pro
+
+             , (max(m.ReleaseDate)                                                           over(partition by wg.SogId, m.AvailabilityId, m.DurationId, m.ReactionTimeId, m.ReactionTypeId, m.ServiceLocationId, m.ProActiveSlaId)) as ReleaseDate
+
+             , m.ListPrice
+             , m.DealerDiscount
+             , m.DealerPrice
+
+        from Hardware.GetCosts(@approved, @cnt, @wg, @av, @dur, @reactiontime, @reactiontype, @loc, @pro, null, null) m
+        join InputAtoms.Wg wg on wg.id = m.WgId and wg.DeactivatedDateTime is null
+        left join Hardware.GetInstallBaseOverSog(@approved, @cnt) ib on ib.Country = m.CountryId and ib.Wg = m.WgId
+    )
+    select    
+            m.Id
+
+            --SLA
+
+            , m.CountryId
+            , m.Country
+            , m.CurrencyId
+            , m.Currency
+            , m.ExchangeRate
+
+            , m.WgId
+            , m.Wg
+            , m.WgDescription
+            , m.SogId
+            , m.Sog
+
+            , m.AvailabilityId
+            , m.Availability
+            , m.DurationId
+            , m.Duration
+            , m.Year
+            , m.IsProlongation
+            , m.ReactionTimeId
+            , m.ReactionTime
+            , m.ReactionTypeId
+            , m.ReactionType
+            , m.ServiceLocationId
+            , m.ServiceLocation
+            , m.ProActiveSlaId
+            , m.ProActiveSla
+            , m.Sla
+            , m.SlaHash
+
+            , m.StdWarranty
+            , m.StdWarrantyLocation
+
+            --Cost
+
+            , m.AvailabilityFee
+            , m.TaxAndDutiesW
+            , m.TaxAndDutiesOow
+            , m.Reinsurance
+            , m.ProActive
+            , m.ServiceSupportCost
+            , m.MaterialW
+            , m.MaterialOow
+            , m.FieldServiceCost
+            , m.Logistic
+            , m.OtherDirect
+            , m.LocalServiceStandardWarranty
+            , m.Credits
+
+            , case when m.sum_ib_x_tc <> 0 and m.sum_ib_by_tc <> 0 then m.sum_ib_x_tc / m.sum_ib_by_tc else 0 end as ServiceTcSog
+            , case when m.sum_ib_x_tp <> 0 and m.sum_ib_by_tp <> 0 then m.sum_ib_x_tp / m.sum_ib_by_tp else 0 end as ServiceTpSog
+            , case when m.sum_ib_x_tp_Released <> 0 and m.sum_ib_by_tp_Released <> 0 then m.sum_ib_x_tp_Released / m.sum_ib_by_tp_Released 
+                   when m.ReleaseDate is not null then 0 end as ServiceTpSog_Released
+
+            , case when m.sum_ib_x_pro <> 0 and m.sum_ib_by_pro <> 0 then m.sum_ib_x_pro / m.sum_ib_by_pro else 0 end as ProActiveSog
+
+            , m.ReleaseDate
+
+            , m.ListPrice
+            , m.DealerDiscount
+            , m.DealerPrice  
+
+    from cte m
+)
+GO
+
+IF OBJECT_ID('Report.ProActive') IS NOT NULL
+  DROP FUNCTION Report.ProActive;
+go 
+
+IF OBJECT_ID('Report.spProActive') IS NOT NULL
+  DROP PROCEDURE Report.spProActive;
+go 
+
+CREATE PROCEDURE Report.spProActive
+(
+    @cnt          bigint,
+    @wg           bigint,
+    @av           bigint,
+    @dur          bigint,
+    @reactiontime bigint,
+    @reactiontype bigint,
+    @loc          bigint,
+    @pro          bigint,
+    @lastid       bigint,
+    @limit        int
+)
+AS
+BEGIN
+
+    declare @cntTable dbo.ListId; insert into @cntTable(id) values(@cnt);
+
+    declare @wg_SOG_Table dbo.ListId;
+    insert into @wg_SOG_Table
+    select id
+        from InputAtoms.Wg 
+        where SogId in (select wg.SogId from InputAtoms.Wg wg where @wg is null or wg.Id = @wg)
+        and IsSoftware = 0
+        and SogId is not null
+        and DeactivatedDateTime is null;
+
+    if not exists(select id from @wg_SOG_Table) return;
+
+    declare @avTable dbo.ListId; if @av is not null insert into @avTable(id) values(@av);
+
+    declare @durTable dbo.ListId; if @dur is not null insert into @durTable(id) values(@dur);
+
+    declare @rtimeTable dbo.ListId; if @reactiontime is not null insert into @rtimeTable(id) values(@reactiontime);
+
+    declare @rtypeTable dbo.ListId; if @reactiontype is not null insert into @rtypeTable(id) values(@reactiontype);
+
+    declare @locTable dbo.ListId; if @loc is not null insert into @locTable(id) values(@loc);
+
+    declare @proTable dbo.ListId; if @pro is not null insert into @proTable(id) values(@pro);
+
+    with cte as (
+        select m.* 
+               , case when m.IsProlongation = 1 then 'Prolongation' else CAST(m.Year as varchar(1)) end as ServicePeriod
+        from Hardware.GetCostsSlaSog(1, @cntTable, @wg_SOG_Table, @avTable, @durTable, @rtimeTable, @rtypeTable, @locTable, @proTable) m
+        where @wg is null or m.WgId = @wg
+    )
+    , cte2 as (
+        select  
+                ROW_NUMBER() over(ORDER BY (SELECT 1)) as rownum
+
+                , m.*
+                , fsp.Name as Fsp
+                , fsp.ServiceDescription as FspDescription
+
+        from cte m
+        left join Fsp.HwFspCodeTranslation fsp on fsp.SlaHash = m.SlaHash and fsp.Sla = m.Sla
+    )
+    select    m.Id
+            , m.Country
+            , c.CountryGroup
+            , m.Fsp
+            , m.Wg
+
+            , m.ServiceLocation
+            , m.ReactionTime
+            , m.ReactionType
+            , m.Availability
+            , m.ProActiveSla
+
+            , m.ServicePeriod as Duration
+
+             , m.ServiceTpSog * m.ExchangeRate as ReActive
+             , m.ProActiveSog * m.ExchangeRate as ProActive
+             , (m.ServiceTpSog + coalesce(m.ProActiveSog, 0)) * m.ExchangeRate as ServiceTP
+
+            , m.Currency
+
+            , wg.Sog
+            , wg.SogDescription
+
+            , m.FspDescription
+    from cte2 m
+    JOIN InputAtoms.CountryView c on c.Id = m.CountryId
+    JOIN InputAtoms.WgSogView wg on wg.Id = m.WgId
+
+    where (@limit is null) or (m.rownum > @lastid and m.rownum <= @lastid + @limit);
+
+END
+GO
+
+update Report.Report set SqlFunc = 'Report.spProActive' where upper(Name) = 'PROACTIVE-REPORTS';
+go
