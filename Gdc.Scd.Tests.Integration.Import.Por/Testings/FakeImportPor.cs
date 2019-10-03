79e9f7d86021ec657fbaf58aaaba3ba295f5fe07
Evgenia Borisova <evgenia.borisova.gdc@ts.fujitsu.com>
2019-10-03 23:36:35 +0300

    Added integration test for por, changed start process for por

---
+++ Gdc.Scd.Tests.Integration.Import.Por/Testings/FakeImportPor.cs
@@ -0,0 +1,21 @@
+﻿using Gdc.Scd.Core.Interfaces;
+using Gdc.Scd.Import.Por;
+using System;
+
+namespace Gdc.Scd.Tests.Integration.Import.Por.Testings
+{
+    public class FakeImportPor : ImportPor
+    {
+        public Exception error;
+
+        public FakeImportPor(ILogger log) : base(log) { }
+
+        public override void Run()
+        {
+            if (error != null)
+            {
+                throw error;
+            }
+        }
+    }
+}
