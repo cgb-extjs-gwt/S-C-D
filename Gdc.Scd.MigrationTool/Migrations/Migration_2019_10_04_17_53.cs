d1ed6047ef1ac0805435fc456a853798b1aae77e
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2019-10-09 13:00:20 +0300

    add migration

--- Gdc.Scd.MigrationTool/Migrations/Migration_2019_10_04_17_53.cs
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_10_04_17_53.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Gdc.Scd.DataAccessLayer.Interfaces;
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
 using Gdc.Scd.MigrationTool.Interfaces;
 
 namespace Gdc.Scd.MigrationTool.Migrations
---
+++ Gdc.Scd.MigrationTool/Migrations/Migration_2019_10_09_12_57.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_10_09_12_57 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 124;
+
+        public string Description => "Fix CD CS, add new columns to service cost sheet";
+
+        public Migration_2019_10_09_12_57(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            this.repositorySet.ExecuteFromFile("2019-10-09-12-57.sql");
+        }
+    }
+}
