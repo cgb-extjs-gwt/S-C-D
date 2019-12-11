699a8a62a9cf36a830e92ea2c0babaa1badace34
Dmitriy Kulikov <Dmitriy.Kulikov.gdc@ts.fujitsu.com>
2019-12-11 20:10:44 +0300

    fix proactive costing overview report

---
+++ Gdc.Scd.MigrationTool/Migrations/2019_12/Migration_2019_12_11_20_10.cs
@@ -0,0 +1,24 @@
+﻿using Gdc.Scd.DataAccessLayer.Interfaces;
+using Gdc.Scd.MigrationTool.Interfaces;
+
+namespace Gdc.Scd.MigrationTool.Migrations
+{
+    public class Migration_2019_12_11_20_10 : IMigrationAction
+    {
+        private readonly IRepositorySet repositorySet;
+
+        public int Number => 132;
+
+        public string Description => "Fix proactive costing overview report";
+
+        public Migration_2019_12_11_20_10(IRepositorySet repositorySet)
+        {
+            this.repositorySet = repositorySet;
+        }
+
+        public void Execute()
+        {
+            this.repositorySet.ExecuteFromFile("2019-12-11-20-10.sql");
+        }
+    }
+}
