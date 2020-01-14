cccf08c832c12a073fe0167c2b853d2a982825c1
Rizvanov Timur <Timur.Rizvanov.gdc@ts.fujitsu.com>
2020-01-14 10:32:42 +0300

    #161308 Filter checkbox highlighting was fixed in portfolio.

--- Gdc.Scd.Web.Client/src/Dict/Components/MultiSelect.tsx
+++ Gdc.Scd.Web.Client/src/Dict/Components/MultiSelect.tsx
@@ -69,6 +69,7 @@ export class MultiSelect extends React.Component<MultiSelectProps, any> {
             });
 
             this.lst.select(records);
+            this.onListClick();
         }
 
         this.props.onSetDefaultValue && this.props.onSetDefaultValue();
