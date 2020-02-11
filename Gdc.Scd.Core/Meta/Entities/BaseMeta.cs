3d226b7e3093b14e283b3325859030f3136c88ea
Rizvanov Timur <Timur.Rizvanov.gdc@ts.fujitsu.com>
2020-02-11 16:36:33 +0300

    #164431 Intermediate data access layer (slice domain meta) was fixed.

--- Gdc.Scd.Core/Meta/Entities/BaseMeta.cs
+++ Gdc.Scd.Core/Meta/Entities/BaseMeta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Gdc.Scd.Core.Meta.Interfaces;
+using Newtonsoft.Json;
 
 namespace Gdc.Scd.Core.Meta.Entities
 {
@@ -7,6 +8,7 @@ namespace Gdc.Scd.Core.Meta.Entities
     {
         public string Id { get; set; }
 
+        [JsonProperty("name")]
         public string Caption { get; set; }
 
         public virtual object Clone()
