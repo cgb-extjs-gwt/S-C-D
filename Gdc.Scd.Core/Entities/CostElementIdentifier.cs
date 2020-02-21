a033d85d017009110249efa271b2420051ec9e21
Rizvanov Timur <Timur.Rizvanov.gdc@ts.fujitsu.com>
2020-02-21 18:20:04 +0300

    #165064 Region input was changed for 'Sar'.

--- Gdc.Scd.Core/Entities/CostElementIdentifier.cs
+++ Gdc.Scd.Core/Entities/CostElementIdentifier.cs
@@ -1,13 +1,19 @@
 ﻿using Gdc.Scd.Core.Interfaces;
+using System;
 
 namespace Gdc.Scd.Core.Entities
 {
-    public class CostElementIdentifier : ICostElementIdentifier
+    public class CostElementIdentifier : ICostElementIdentifier, ICloneable
     {
         public string CostElementId { get; set; }
 
         public string ApplicationId { get; set; }
 
         public string CostBlockId { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
     }
 }
