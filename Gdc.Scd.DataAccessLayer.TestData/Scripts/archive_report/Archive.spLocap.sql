9d9fe90b3aba441a84c436e42e6109fae8aa46b3
Dmitriy Kulikov <dmitriy.kulikov.gdc@ts.fujitsu.com>
2020-05-15 15:52:50 +0300

    add new archives, fix archive calls

---
+++ Gdc.Scd.DataAccessLayer.TestData/Scripts/archive_report/Archive.spLocap.sql
@@ -0,0 +1,13 @@
+﻿if OBJECT_ID('Archive.spLocap') is not null
+    drop procedure Archive.spLocap
+go
+
+create procedure Archive.spLocap(
+    @cnt bigint
+)
+as
+BEGIN
+    declare @wg dbo.ListID ;
+    exec Report.spLocapDetailedApproved @cnt, @wg, null, null, null, null, null, null, null;
+END
+go
--- Gdc.Scd.Tests.Integration.Export.Archive/ArchiveRepositoryTest.cs
+++ Gdc.Scd.Tests.Integration.Export.Archive/ArchiveRepositoryTest.cs
@@ -1,12 +1,17 @@
 ﻿using NUnit.Framework;
 using System;
 using Gdc.Scd.Export.ArchiveJob.Impl;
+using Gdc.Scd.Tests.Util;
+using Gdc.Scd.Export.ArchiveJob.Dto;
 
 namespace Gdc.Scd.Tests.Integration.Export.Archive
 {
     public class ArchiveRepositoryTest : ArchiveRepository
     {
-        public ArchiveRepositoryTest() : base(null) { }
+        public ArchiveRepositoryTest() : base(null)
+        {
+            this.path = StreamUtil.Location();
+        }
 
         [TestCase(@"\\some\path\to\", 1917, 11, 7, @"\\some\path\to\1917-11-07")]
         [TestCase(@"not/a/real/path/", 1945, 5, 9, @"not/a/real/path/1945-05-09")]
@@ -16,6 +21,16 @@ namespace Gdc.Scd.Tests.Integration.Export.Archive
             this.timestamp = new DateTime(year, month, day);
             this.path = path;
             Assert.AreEqual(expected, GetPath());
+        }
+
+        [TestCase]
+        public void SaveCountryArchive_Test()
+        {
+            var excel = StreamUtil.ReadBin("TestData", "hdd.xlsx");
+            var cnt = new CountryDto { Id = -1, Name = "Fake country", ISO = "no name" };
+            var archive = new ArchiveDto { ArchiveName = "Hdd retention here" };
+
+            Save(cnt, archive, excel);
         }
     }
 }
