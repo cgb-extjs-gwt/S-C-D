154d8e57c74b1adf0f02312341c797a6314fad97
Rizvanov Timur <Timur.Rizvanov.gdc@ts.fujitsu.com>
2020-05-20 18:53:43 +0300

    #178069 TableView. Excel export. Server side.
    (cherry picked from commit d6cdb9543b15cded7df83cb3cd7bdb94d54c2743)

---
+++ Gdc.Scd.BusinessLogicLayer/Interfaces/IExcelConverterService.cs
@@ -0,0 +1,11 @@
+﻿using Gdc.Scd.Core.Meta.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Gdc.Scd.BusinessLogicLayer.Interfaces
+{
+    public interface IExcelConverterService
+    {
+        Task<Func<string, object>> BuildConverter(CostBlockEntityMeta meta, string costElementId);
+    }
+}
