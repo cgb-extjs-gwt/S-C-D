0f4c1aae3e9cf699938474aaf2acf68d1fde58e2
Venera Faizullina <venera.faizullina.gdc@ts.fujitsu.com>
2020-05-26 23:14:40 +0300

    immediate fix to sap upload app

--- Gdc.Scd.Export.Sap/Config.cs
+++ Gdc.Scd.Export.Sap/Config.cs
@@ -5,6 +5,7 @@ namespace Gdc.Scd.Export.Sap
     public static class Config
     {
         public static string SapFileName => ConfigurationManager.AppSettings["SapFileName"];
+        public static string BiSessionName => ConfigurationManager.AppSettings["BiSessionName"];
         public static string ExportDirectory => ConfigurationManager.AppSettings["ExportDirectory"];
         public static string ExportHost => ConfigurationManager.AppSettings["ExportHost"];
         public static string Admission => ConfigurationManager.AppSettings["Admission"];
